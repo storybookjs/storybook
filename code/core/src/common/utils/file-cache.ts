24ea0ad557d6d17b191d84f23501df1b4a13888a
Norbert de Langen <ndelangen@me.com>
2024-08-03 00:13:53 +0200

    fix

--- code/core/src/common/utils/file-cache.ts
+++ code/core/src/common/utils/file-cache.ts
@@ -1,7 +1,6 @@
-import * as fsc from 'file-system-cache';
+import fsc from 'file-system-cache';
 
-// @ts-expect-error (needed due to it's use of `exports.default`)
-const Cache = (fsc.default.default || fsc.default) as typeof fsc.default;
+const Cache = fsc;
 
 export type Options = Parameters<typeof Cache>['0'];
 export type FileSystemCache = ReturnType<typeof Cache>;

27b0b022c70f89026963dddb0fdf8ef156c319a6
Norbert de Langen <ndelangen@me.com>
2024-08-03 23:30:57 +0200

    fix sveltekit

--- code/frameworks/sveltekit/src/preset.ts
+++ code/frameworks/sveltekit/src/preset.ts
@@ -1,7 +1,6 @@
 import { viteFinal as svelteViteFinal } from '@storybook/svelte-vite/preset';
 import type { PresetProperty } from 'storybook/internal/types';
-import { withoutVitePlugins } from '@storybook/builder-vite';
-import { dirname, join } from 'path';
+import { dirname, join } from 'node:path';
 import { configOverrides } from './plugins/config-overrides';
 import { mockSveltekitStores } from './plugins/mock-sveltekit-stores';
 import { type StorybookConfig } from './types';
@@ -22,6 +21,8 @@ export const viteFinal: NonNullable<StorybookConfig['viteFinal']> = async (confi
   const baseConfig = await svelteViteFinal(config, options);
 
   let { plugins = [] } = baseConfig;
+
+  const { withoutVitePlugins } = await import('@storybook/builder-vite');
 
   // disable specific plugins that are not compatible with Storybook
   plugins = (

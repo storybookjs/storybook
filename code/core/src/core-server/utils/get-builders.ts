52329e878b5df1b06df3b12184656a0102a6e66f
Norbert de Langen <ndelangen@me.com>
2024-08-03 22:06:33 +0200

    make builders use ESM if possible

--- code/core/src/core-server/utils/get-builders.ts
+++ code/core/src/core-server/utils/get-builders.ts
@@ -1,6 +1,8 @@
 import type { Builder, Options } from '@storybook/core/types';
 import { MissingBuilderError } from '@storybook/core/server-errors';
 import { pathToFileURL } from 'node:url';
+import { isAbsolute, join } from 'node:path';
+import * as resolve from 'resolve.exports';
 
 export async function getManagerBuilder(): Promise<Builder<unknown>> {
   return import('@storybook/core/builder-manager');
@@ -10,11 +12,31 @@ export async function getPreviewBuilder(
   builderName: string,
   configDir: string
 ): Promise<Builder<unknown>> {
-  const builderPackage = require.resolve(
-    ['webpack5'].includes(builderName) ? `@storybook/builder-${builderName}` : builderName,
-    { paths: [configDir] }
-  );
-  const previewBuilder = await import(pathToFileURL(builderPackage).href);
+  let builderPackage = builderName;
+
+  if (builderName === 'webpack5') {
+    builderPackage = '@storybook/builder-webpack5';
+  }
+
+  try {
+    const pkg = require(join(builderPackage, 'package.json'));
+    const subpath = resolve.exports(pkg, '.');
+
+    if (subpath) {
+      builderPackage = join(builderPackage, ...subpath);
+    }
+  } catch (err) {
+    // failed = true;
+  }
+
+  builderPackage = require.resolve(builderName);
+
+  if (isAbsolute(builderName)) {
+    builderPackage = pathToFileURL(builderPackage).href;
+  }
+
+  const previewBuilder = await import(builderPackage);
+  console.log({ builderPackage, href: pathToFileURL(builderPackage).href });
   return previewBuilder;
 }
 

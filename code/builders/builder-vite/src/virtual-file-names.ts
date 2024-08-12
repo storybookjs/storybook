db593fa6ba419b541686cf57fab9ed08a0203b46
Valentin Palkovic <valentin@chromatic.com>
2024-08-12 16:51:00 +0200

    Builder-Vite: Add null character prefix to virtual file IDs in code generator plugin

--- code/builders/builder-vite/src/virtual-file-names.ts
+++ code/builders/builder-vite/src/virtual-file-names.ts
@@ -1,4 +1,4 @@
-export const virtualFileId = '/virtual:/@storybook/builder-vite/vite-app.js';
-export const virtualStoriesFile = '/virtual:/@storybook/builder-vite/storybook-stories.js';
-export const virtualPreviewFile = '/virtual:/@storybook/builder-vite/preview-entry.js';
-export const virtualAddonSetupFile = '/virtual:/@storybook/builder-vite/setup-addons.js';
+export const virtualFileId = '\0virtual:/@storybook/builder-vite/vite-app.js';
+export const virtualStoriesFile = '\0virtual:/@storybook/builder-vite/storybook-stories.js';
+export const virtualPreviewFile = '\0virtual:/@storybook/builder-vite/preview-entry.js';
+export const virtualAddonSetupFile = '\0virtual:/@storybook/builder-vite/setup-addons.js';

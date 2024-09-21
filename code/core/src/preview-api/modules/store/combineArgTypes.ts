feb64d1b191ff22fa96a43a7d2baef10d3efae43
hakshu25 <hakshu25@gmail.com>
2024-09-21 23:30:49 +0900

    refactor: Simplify combineArgTypes function

--- code/core/src/preview-api/modules/store/combineArgTypes.ts
+++ code/core/src/preview-api/modules/store/combineArgTypes.ts
@@ -6,11 +6,12 @@ export const combineArgTypes = (
   ...argTypes: (Partial<ArgTypes> | StrictArgTypes | undefined)[]
 ): StrictArgTypes => {
   const combinedArgTypes = combineParameters(...argTypes);
-  return Object.entries(combinedArgTypes).reduce((acc, [key, argType]) => {
-    if (argType.control?.type && argType.control?.disable) {
-      delete argType.control.disable;
-    }
-    acc[key] = argType;
-    return acc;
-  }, {} as StrictArgTypes);
+  return Object.fromEntries(
+    Object.entries(combinedArgTypes).map(([key, argType]) => {
+      if (argType.control?.type && argType.control?.disable) {
+        delete argType.control.disable;
+      }
+      return [key, argType];
+    })
+  );
 };

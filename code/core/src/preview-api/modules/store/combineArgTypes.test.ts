e6399df2c27e3911dff93102788c5a86646da3c9
hakshu25 <hakshu25@gmail.com>
2024-09-22 01:05:55 +0900

    fix: fixed parameters types and condition

--- code/core/src/preview-api/modules/store/combineArgTypes.test.ts
+++ code/core/src/preview-api/modules/store/combineArgTypes.test.ts
@@ -6,34 +6,75 @@ import { combineArgTypes } from './combineArgTypes';
 
 describe('combineArgTypes', () => {
   it('should combine argTypes', () => {
-    const argTypes1: Partial<ArgTypes> = {
+    const projectArgTypes: Partial<ArgTypes> = {
       a: { name: 'a', control: { type: 'text' } },
       b: { name: 'b', control: { type: 'boolean' } },
       d: { name: 'd', control: { disable: true } },
     };
-    const argTypes2: Partial<ArgTypes> = {
+    const componentArgTypes: Partial<ArgTypes> = {
       a: { name: 'a', control: { type: 'text' } },
       c: { name: 'c', control: { type: 'text' } },
+      e: { name: 'e', control: { disable: true } },
     };
-    const combinedArgTypes = combineArgTypes(argTypes1, argTypes2);
+    const storyArgTypes: Partial<ArgTypes> = {
+      f: { name: 'f', control: { type: 'text' } },
+    };
+    const combinedArgTypes = combineArgTypes(projectArgTypes, componentArgTypes, storyArgTypes);
     expect(combinedArgTypes).toEqual({
       a: { name: 'a', control: { type: 'text' } },
       b: { name: 'b', control: { type: 'boolean' } },
       c: { name: 'c', control: { type: 'text' } },
       d: { name: 'd', control: { disable: true } },
+      e: { name: 'e', control: { disable: true } },
+      f: { name: 'f', control: { type: 'text' } },
     });
   });
 
-  it('should remove disable property if control type is defined', () => {
-    const argTypes1: Partial<ArgTypes> = {
+  it('should enable control if story is enable', () => {
+    const projectArgTypes: Partial<ArgTypes> = {
+      a: { name: 'a', control: { type: 'text' } },
+    };
+    const componentArgTypes: Partial<ArgTypes> = {
       a: { name: 'a', control: { type: 'text', disable: true } },
     };
-    const argTypes2: Partial<ArgTypes> = {
+    const storyArgTypes: Partial<ArgTypes> = {
       a: { name: 'a', control: { type: 'text' } },
     };
-    const combinedArgTypes = combineArgTypes(argTypes1, argTypes2);
+    const combinedArgTypes = combineArgTypes(projectArgTypes, componentArgTypes, storyArgTypes);
     expect(combinedArgTypes).toEqual({
       a: { name: 'a', control: { type: 'text' } },
+    });
+  });
+
+  it('should disable control if story is disable', () => {
+    const projectArgTypes: Partial<ArgTypes> = {
+      a: { name: 'a', control: { type: 'text' } },
+    };
+    const componentArgTypes: Partial<ArgTypes> = {
+      a: { name: 'a', control: { type: 'text' } },
+    };
+    const storyArgTypes: Partial<ArgTypes> = {
+      a: { name: 'a', control: { type: 'text', disable: true } },
+    };
+    const combinedArgTypes = combineArgTypes(projectArgTypes, componentArgTypes, storyArgTypes);
+    expect(combinedArgTypes).toEqual({
+      a: { name: 'a', control: { type: 'text', disable: true } },
+    });
+  });
+
+  it('should enable control if component is disable and story is enable', () => {
+    const projectArgTypes: Partial<ArgTypes> = {
+      a: { name: 'a', control: { type: 'text' } },
+    };
+    const componentArgTypes: Partial<ArgTypes> = {
+      a: { name: 'a', control: { type: 'text', disable: true } },
+    };
+    const storyArgTypes: Partial<ArgTypes> = {
+      a: { name: 'a', control: { type: 'text', disable: false } },
+    };
+    const combinedArgTypes = combineArgTypes(projectArgTypes, componentArgTypes, storyArgTypes);
+    expect(combinedArgTypes).toEqual({
+      a: { name: 'a', control: { type: 'text', disable: false } },
     });
   });
 });
--- code/core/src/preview-api/modules/store/combineArgTypes.ts
+++ code/core/src/preview-api/modules/store/combineArgTypes.ts
@@ -3,12 +3,21 @@ import type { ArgTypes, StrictArgTypes } from '@storybook/csf';
 import { combineParameters } from './parameters';
 
 export const combineArgTypes = (
-  ...argTypes: (Partial<ArgTypes> | StrictArgTypes | undefined)[]
+  projectArgTypes: Partial<ArgTypes> | StrictArgTypes | undefined,
+  componentArgTypes: Partial<ArgTypes> | StrictArgTypes | undefined,
+  storyArgTypes: Partial<ArgTypes> | StrictArgTypes | undefined
 ): StrictArgTypes => {
-  const combinedArgTypes = combineParameters(...argTypes);
+  const combinedArgTypes = combineParameters(projectArgTypes, componentArgTypes, storyArgTypes);
   return Object.fromEntries(
     Object.entries(combinedArgTypes).map(([key, argType]) => {
-      if (argType.control?.type && argType.control?.disable) {
+      const componentArgType: any = componentArgTypes?.[key];
+      const storyArgType: any = storyArgTypes?.[key];
+      if (
+        argType?.control?.disable &&
+        componentArgType?.control?.disable &&
+        storyArgType?.control?.type &&
+        !storyArgType?.control?.disable
+      ) {
         delete argType.control.disable;
       }
       return [key, argType];

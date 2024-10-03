69fc2dc87b92ab297a2ac635db82599edc7c230c
Yann Braga <yannbf@gmail.com>
2024-10-03 10:21:59 +0200

    debug

--- code/.storybook/vitest.config.ts
+++ code/.storybook/vitest.config.ts
@@ -16,7 +16,6 @@ if (process.env.INSPECT === 'true') {
     })
   );
 }
-
 export default mergeConfig(
   vitestCommonConfig,
   // @ts-expect-error added this because of testNamePattern below
@@ -36,7 +35,28 @@ export default mergeConfig(
       name: 'storybook-ui',
       include: [
         '../addons/**/*.{story,stories}.?(c|m)[jt]s?(x)',
-        // '../core/template/stories/**/*.{story,stories}.?(c|m)[jt]s?(x)',
+        // '../core/template/stories/argMapping.{story,stories}.?(c|m)[jt]s?(x)',
+        // '../core/template/stories/args.{story,stories}.?(c|m)[jt]s?(x)',
+        // '../core/template/stories/argTypes.{story,stories}.?(c|m)[jt]s?(x)',
+        // '../core/template/stories/autotitle.{story,stories}.?(c|m)[jt]s?(x)',
+        // '../core/template/stories/component-play.{story,stories}.?(c|m)[jt]s?(x)',
+        '../core/template/stories/decorators.{story,stories}.?(c|m)[jt]s?(x)',
+        '../core/template/stories/exportOrder.{story,stories}.?(c|m)[jt]s?(x)',
+        '../core/template/stories/globals.{story,stories}.?(c|m)[jt]s?(x)',
+        '../core/template/stories/hooks.{story,stories}.?(c|m)[jt]s?(x)',
+        '../core/template/stories/indexer.{story,stories}.?(c|m)[jt]s?(x)',
+        '../core/template/stories/interleavedExports.{story,stories}.?(c|m)[jt]s?(x)',
+        // '../core/template/stories/layout.{story,stories}.?(c|m)[jt]s?(x)',
+        // '../core/template/stories/loaders.{story,stories}.?(c|m)[jt]s?(x)',
+        // '../core/template/stories/names.{story,stories}.?(c|m)[jt]s?(x)',
+        // '../core/template/stories/parameters.{story,stories}.?(c|m)[jt]s?(x)',
+        // '../core/template/stories/rendering.{story,stories}.?(c|m)[jt]s?(x)',
+        // '../core/template/stories/shortcuts.{story,stories}.?(c|m)[jt]s?(x)',
+        // '../core/template/stories/tags-add.{story,stories}.?(c|m)[jt]s?(x)',
+        // '../core/template/stories/tags-config.{story,stories}.?(c|m)[jt]s?(x)',
+        // '../core/template/stories/tags-remove.{story,stories}.?(c|m)[jt]s?(x)',
+        // '../core/template/stories/title.{story,stories}.?(c|m)[jt]s?(x)',
+        // '../core/template/stories/unicode.{story,stories}.?(c|m)[jt]s?(x)',
         '../core/src/manager/**/*.{story,stories}.?(c|m)[jt]s?(x)',
         '../core/src/preview-api/**/*.{story,stories}.?(c|m)[jt]s?(x)',
         '../core/src/components/{brand,components}/**/*.{story,stories}.?(c|m)[jt]s?(x)',

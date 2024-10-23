946e8615bebcf0427e7921f103972faa31511afb
Kendall Gassner <kendallgassner@github.com>
2024-10-23 18:35:08 +0000

    By default disable interactions for users with prefers-reduced-motion

--- code/addons/interactions/src/components/InteractionToggle.tsx
+++ code/addons/interactions/src/components/InteractionToggle.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useEffect } from 'react';
 
 import { IconButton } from 'storybook/internal/components';
 
@@ -14,9 +14,20 @@ export const InteractionToggle = () => {
     window?.localStorage.getItem(INTERACTION_STORAGE_KEY) === 'true'
   );
 
+  useEffect(() => {
+    const reducedMotion = matchMedia('(prefers-reduced-motion)');
+
+    if (window?.localStorage.getItem(INTERACTION_STORAGE_KEY) === null && reducedMotion.matches) {
+      window?.localStorage?.setItem(INTERACTION_STORAGE_KEY, 'true');
+      setDisableInteractions(true);
+    }
+  }, []);
+
   const toggleMyTool = useCallback(() => {
     window?.localStorage?.setItem(INTERACTION_STORAGE_KEY, `${!disableInteractions}`);
     setDisableInteractions(!disableInteractions);
+    // Refreshes the page to cause the interaction to stop/start
+    window.location.reload();
   }, [disableInteractions, setDisableInteractions]);
 
   return (
@@ -28,7 +39,7 @@ export const InteractionToggle = () => {
       aria-pressed={disableInteractions}
     >
       {disableInteractions ? <PlayIcon /> : <StopIcon />}
-      <span>Interactions</span>
+      Interactions
     </IconButton>
   );
 };

5a8344d5a82d5575aa27262b8a1783d81f6500e5
Steve Dodier-Lazaro <sidnioulz@gmail.com>
2024-11-04 08:57:29 +0100

    fix: Improve typing of addon config for layout customisations

--- code/core/src/types/modules/addons.ts
+++ code/core/src/types/modules/addons.ts
@@ -5,7 +5,7 @@ import type { TestProviderConfig, TestingModuleProgressReportProgress } from '..
 import type { State } from '../../manager-api';
 import type { RenderData as RouterData } from '../../router/types';
 import type { ThemeVars } from '../../theming/types';
-import type { API_SidebarOptions } from './api';
+import type { API_LayoutCustomisations, API_SidebarOptions } from './api';
 import type { API_HashEntry, API_StoryEntry } from './api-stories';
 import type {
   Args,
@@ -539,8 +539,8 @@ export interface Addon_ToolbarConfig {
 export interface Addon_Config {
   theme?: ThemeVars;
   layout?: {
-    showSidebar?: (state: State, currentValue: boolean) => boolean;
-    showToolbar?: (state: State, currentValue: boolean) => boolean;
+    showSidebar?: API_LayoutCustomisations['showSidebar'];
+    showToolbar?: API_LayoutCustomisations['showToolbar'];
   };
   toolbar?: {
     [id: string]: Addon_ToolbarConfig;

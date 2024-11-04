b967f8cdef74703dc1b59c71a07b76d490081b8a
Steve Dodier-Lazaro <sidnioulz@gmail.com>
2024-11-04 08:54:57 +0100

    refactor: Improve internal variable naming in manager preview

--- code/core/src/manager/components/preview/Preview.tsx
+++ code/core/src/manager/components/preview/Preview.tsx
@@ -60,7 +60,7 @@ const Preview = React.memo<PreviewProps>(function Preview(props) {
 
   const shouldScale = viewMode === 'story';
   const { showToolbar } = options;
-  const shouldShowToolbar = api.getShowToolbarWithCustomisations(showToolbar);
+  const customisedShowToolbar = api.getShowToolbarWithCustomisations(showToolbar);
 
   const previousStoryId = useRef(storyId);
 
@@ -95,7 +95,7 @@ const Preview = React.memo<PreviewProps>(function Preview(props) {
         <S.PreviewContainer>
           <ToolbarComp
             key="tools"
-            isShown={shouldShowToolbar}
+            isShown={customisedShowToolbar}
             // @ts-expect-error (non strict)
             tabId={tabId}
             tabs={tabs}

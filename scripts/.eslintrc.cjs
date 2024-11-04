30ca58583f82f9e2c8909bffc07760d2668a2695
Yann Braga <yannbf@gmail.com>
2024-11-04 13:09:51 +0100

    add oxlint

--- scripts/.eslintrc.cjs
+++ scripts/.eslintrc.cjs
@@ -1,7 +1,7 @@
 module.exports = {
   root: true,
   extends: [
-    //
+    "plugin:oxlint/recommended",
     '@storybook/eslint-config-storybook',
     'plugin:storybook/recommended',
     'plugin:depend/recommended'

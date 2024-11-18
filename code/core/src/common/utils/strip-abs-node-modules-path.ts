4154c3cb755b55007ddca50b265e1a39fdbf5a09
Valentin Palkovic <valentin@chromatic.com>
2024-11-18 13:13:10 +0100

    Preview: Fix path resolution in yarn pnp node-linker=pnpm mode

--- code/core/src/common/utils/strip-abs-node-modules-path.ts
+++ code/core/src/common/utils/strip-abs-node-modules-path.ts
@@ -1,4 +1,4 @@
-import { posix, sep } from 'node:path';
+import { join, posix, sep } from 'node:path';
 
 import slash from 'slash';
 
@@ -9,6 +9,10 @@ function normalizePath(id: string) {
 // We need to convert from an absolute path, to a traditional node module import path,
 // so that vite can correctly pre-bundle/optimize
 export function stripAbsNodeModulesPath(absPath: string) {
+  // If the environment is a yarn pnp node_linker=pnpm environment, we don't want to strip the path
+  if (absPath.includes(join('node_modules', '.cache'))) {
+    return absPath;
+  }
   // TODO: Evaluate if searching for node_modules in a yarn pnp environment is correct
   const splits = absPath.split(`node_modules${sep}`);
   // Return everything after the final "node_modules/"

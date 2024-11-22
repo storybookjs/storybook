078261034ada4160d0fef1129d31fd023795874f
Steve Dodier-Lazaro <sidnioulz@gmail.com>
2024-11-22 11:58:27 +0100

    chore: Remove layout customisations from internal SB instance config

--- code/.storybook/manager.tsx
+++ code/.storybook/manager.tsx
@@ -5,20 +5,4 @@ addons.setConfig({
   sidebar: {
     renderLabel: ({ name, type }) => (type === 'story' ? name : startCase(name)),
   },
-  layoutCustomisations: {
-    showSidebar(state: State, defaultValue: boolean) {
-      if (state.viewMode === 'story' && state.storyId.startsWith('😀')) {
-        return false;
-      }
-
-      return defaultValue;
-    },
-    showToolbar(state: State, defaultValue: boolean) {
-      if (state.viewMode === 'docs') {
-        return false;
-      }
-
-      return defaultValue;
-    },
-  },
 });

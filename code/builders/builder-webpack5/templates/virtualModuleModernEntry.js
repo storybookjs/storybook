d22748c8e0ece832c5ce6d4215b2b92fa5921375
aka <akadev8@outlook.com>
2024-11-28 06:54:27 +0100

    Update virtualModuleModernEntry.js
    Signed-off-by: aka <akadev8@outlook.com>

--- code/builders/builder-webpack5/templates/virtualModuleModernEntry.js
+++ code/builders/builder-webpack5/templates/virtualModuleModernEntry.js
@@ -2,9 +2,9 @@ import { createBrowserChannel } from 'storybook/internal/channels';
 import { PreviewWeb, addons, composeConfigs } from 'storybook/internal/preview-api';
 
 import { global } from '@storybook/global';
-import { importFn } from '{{storiesFilename}}';
+import { importFn } from './stories/index.js';
 
-const getProjectAnnotations = () => composeConfigs(['{{previewAnnotations_requires}}']);
+const getProjectAnnotations = () => composeConfigs(['./preview.js']);
 
 const channel = createBrowserChannel({ page: 'preview' });
 addons.setChannel(channel);
@@ -19,12 +19,12 @@ window.__STORYBOOK_STORY_STORE__ = preview.storyStore;
 window.__STORYBOOK_ADDONS_CHANNEL__ = channel;
 
 if (import.meta.webpackHot) {
-  import.meta.webpackHot.accept('{{storiesFilename}}', () => {
+  import.meta.webpackHot.accept('./stories/index.js', () => {
     // importFn has changed so we need to patch the new one in
     preview.onStoriesChanged({ importFn });
   });
 
-  import.meta.webpackHot.accept(['{{previewAnnotations}}'], () => {
+  import.meta.webpackHot.accept(['./preview.js'], () => {
     // getProjectAnnotations has changed so we need to patch the new one in
     preview.onGetProjectAnnotationsChanged({ getProjectAnnotations });
   });

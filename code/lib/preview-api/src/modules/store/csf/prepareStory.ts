0c198ac17140274cbc72a07be5bbdb9e5970c248
bochka <bochka@mayflower.work>
2024-12-18 10:47:56 +0200

    simplify logic

--- code/lib/preview-api/src/modules/store/csf/prepareStory.ts
+++ code/lib/preview-api/src/modules/store/csf/prepareStory.ts
@@ -145,15 +145,10 @@ function preparePartialAnnotations<TRenderer extends Renderer>(
   // anything at render time. The assumption is that as we don't load all the stories at once, this
   // will have a limited cost. If this proves misguided, we can refactor it.
 
-  const tags = [];
-
-  if (storyAnnotations?.tags?.length) {
-    tags.push(...storyAnnotations.tags);
-  } else if (componentAnnotations.tags?.length) {
-    tags.push(...componentAnnotations.tags);
-  }
-
-  tags.push('story');
+  const tags = [
+    ...(storyAnnotations?.tags?.length ? storyAnnotations.tags : componentAnnotations.tags || []),
+    'story',
+  ];
 
   const parameters: Parameters = combineParameters(
     projectAnnotations.parameters,

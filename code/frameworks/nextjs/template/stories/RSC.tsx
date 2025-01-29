3285bd6a1914bdecd51c4be68d093d5622ad4891
NEKOYASAN <hello@nekoyasan.me>
2025-01-30 03:34:42 +0900

    Add Next.js RSC template with Async Function inside

--- code/frameworks/nextjs/template/stories/RSC.tsx
+++ code/frameworks/nextjs/template/stories/RSC.tsx
@@ -5,3 +5,9 @@ import 'server-only';
 export const RSC = async ({ label }: { label: string }) => <>RSC {label}</>;
 
 export const Nested = async ({ children }: any) => <>Nested {children}</>;
+
+export const WithAsyncFunction = async ({ children }: { children: any }) => {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+
+  return <>Wait 1sec {children}</>;
+};

bd4370161aabbc18938c77e5acd079686ff6aab2
Jeppe Reinhold <jeppe@reinhold.is>
2025-02-21 11:05:18 +0100

    cleanup

--- code/builders/builder-vite/src/codegen-importfn-script.ts
+++ code/builders/builder-vite/src/codegen-importfn-script.ts
@@ -18,7 +18,6 @@ export async function generateImportFnScriptCode(index: StoryIndex): Promise<str
 
   const objectEntries: [string, string][] = uniqueImportPaths.map((importPath) => {
     if (importPath.startsWith('virtual:')) {
-      console.log('LOG: virtual entry', importPath);
       return [importPath, genDynamicImport(importPath)];
     }
 

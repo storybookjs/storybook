228b5dded72a90c705a5092b6b4ab3613600ede9
Valentin Palkovic <valentin@chromatic.com>
2025-02-27 15:01:46 +0100

    Svelte: Filter out Docgen event props in SlotDecorator

--- code/renderers/svelte/src/components/SlotDecorator.svelte
+++ code/renderers/svelte/src/components/SlotDecorator.svelte
@@ -6,14 +6,23 @@
     props = {}
   } = $props();
 
+  /*
+    Svelte Docgen will create argTypes for events with the name 'event_eventName'
+    The Actions addon will convert these to args because they are type: 'action'
+    We need to filter these args out so they are not passed to the component
+  */
+  let propsWithoutDocgenEvents = $derived(Object.fromEntries(
+    Object.entries(props).filter(([key]) => !key.startsWith('event_'))
+  ));
+
   let instance = $state();
   let decoratorInstance = $state();
 </script>
 
 {#if Decorator}
   <Decorator {...decoratorProps} bind:this={decoratorInstance}>
-    <Component {...props} bind:this={instance}/>
+    <Component {...propsWithoutDocgenEvents} bind:this={instance}/>
   </Decorator>
 {:else}
-  <Component {...props} bind:this={instance}/>
+  <Component {...propsWithoutDocgenEvents} bind:this={instance}/>
 {/if}

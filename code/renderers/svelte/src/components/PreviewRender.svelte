7c9512ef4d10e633c36d323ddf5f726423ed8422
Valentin Palkovic <valentin@chromatic.com>
2025-02-27 16:16:54 +0100

    Revert changes in PreviewRender.svelte

--- code/renderers/svelte/src/components/PreviewRender.svelte
+++ code/renderers/svelte/src/components/PreviewRender.svelte
@@ -2,34 +2,41 @@
   import SlotDecorator from './SlotDecorator.svelte';
   import { dedent } from 'ts-dedent';
 
-  const { name, title, storyFn, showError } = $props();
+  export let name;
+  export let title;
+  export let storyFn;
+  export let showError;
+  export let storyContext;
 
-  let Component = $state();
-  let componentnProps = $state({});
-  let on = $state();
-  let argTypes = $state();
-  let firstTime = $state(true);
+  let {
+    /** @type {SvelteComponent} */
+    Component,
+    /** @type {any} */
+    props = {},
+    /** @type {{[string]: () => {}}} Attach svelte event handlers */
+    on,
+    /** @type {any} whether this level of the decorator chain is the last, ie. the actual story */
+    argTypes,
+  } = storyFn();
+  let firstTime = true;
 
-  // Initial load
-  const initialResult = storyFn();
-  Component = initialResult.Component;
-  componentnProps = initialResult.props || {};
-  on = initialResult.on;
-  argTypes = initialResult.argTypes;
-
-  // Re-evaluate when storyFn changes
-  $effect(() => {
+  // the first time we don't want to call storyFn two times so we just return the values
+  // we already have from the previous call. If storyFn changes this function will run
+  // again but this time firstTime will be false
+  function getStoryFnValue(storyFn) {
     if (firstTime) {
       firstTime = false;
-      return;
+      return {
+        Component,
+        props,
+        on,
+        argTypes,
+      };
     }
-    
-    const result = storyFn();
-    Component = result.Component;
-    componentnProps = result.props || {};
-    on = result.on;
-    argTypes = result.argTypes;
-  });
+    return storyFn();
+  }
+  // reactive, re-render on storyFn change
+  $: ({ Component, props = {}, on, argTypes } = getStoryFnValue(storyFn));
 
   // set the argTypes context, read by the last SlotDecorator that renders the original story
   if (!Component) {
@@ -44,4 +51,4 @@
   }
 </script>
 
-<SlotDecorator {Component} props={componentnProps} {on} {argTypes} />
+<SlotDecorator {Component} props={props} {on} {argTypes} />

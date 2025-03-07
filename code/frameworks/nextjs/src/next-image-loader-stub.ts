ac3fc425a103fdace27ac622b81d276c5f7b5834
Kasper Peulen <kasperpeulen@gmail.com>
2025-03-07 12:43:16 +0100

    Revert "Next.js: Upgrade image-size to 2.0"

--- code/frameworks/nextjs/src/next-image-loader-stub.ts
+++ code/frameworks/nextjs/src/next-image-loader-stub.ts
@@ -1,4 +1,8 @@
-import { imageSize } from 'image-size';
+import { cpus } from 'node:os';
+
+import { NextJsSharpError } from 'storybook/internal/preview-errors';
+
+import imageSizeOf from 'image-size';
 import { interpolateName } from 'loader-utils';
 import type { NextConfig } from 'next';
 import type { RawLoaderDefinition } from 'webpack';
@@ -6,6 +10,21 @@ import type { RawLoaderDefinition } from 'webpack';
 interface LoaderOptions {
   filename: string;
   nextConfig: NextConfig;
+}
+
+let sharp: typeof import('sharp') | undefined;
+
+try {
+  sharp = require('sharp');
+  if (sharp && sharp.concurrency() > 1) {
+    // Reducing concurrency reduces the memory usage too.
+    const divisor = process.env.NODE_ENV === 'development' ? 4 : 2;
+    sharp.concurrency(Math.floor(Math.max(cpus().length / divisor, 1)));
+  }
+} catch (e) {
+  console.warn(
+    'You have to install sharp in order to use image optimization features in Next.js. AVIF support is also disabled.'
+  );
 }
 
 const nextImageLoaderStub: RawLoaderDefinition<LoaderOptions> = async function NextImageLoader(
@@ -17,6 +36,7 @@ const nextImageLoaderStub: RawLoaderDefinition<LoaderOptions> = async function N
     content,
   };
   const outputPath = interpolateName(this, filename.replace('[ext]', '.[ext]'), opts);
+  const extension = interpolateName(this, '[ext]', opts);
 
   this.emitFile(outputPath, content);
 
@@ -24,7 +44,23 @@ const nextImageLoaderStub: RawLoaderDefinition<LoaderOptions> = async function N
     return `const src = '${outputPath}'; export default src;`;
   }
 
-  const { width, height } = imageSize(content as Uint8Array);
+  let width;
+  let height;
+
+  if (extension === 'avif') {
+    if (sharp) {
+      const transformer = sharp(content);
+      const result = await transformer.metadata();
+      width = result.width;
+      height = result.height;
+    } else {
+      throw new NextJsSharpError();
+    }
+  } else {
+    const result = imageSizeOf(this.resourcePath);
+    width = result.width;
+    height = result.height;
+  }
 
   return `export default ${JSON.stringify({
     src: outputPath,

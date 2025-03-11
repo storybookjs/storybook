6ae98b602f4c963a6f8e5530e139ac9085443d28
Norbert de Langen <ndelangen@me.com>
2025-03-11 12:18:04 +0100

    Essentials Addon: Update import paths from internal modules to direct package imports

--- code/addons/essentials/src/preview.ts
+++ code/addons/essentials/src/preview.ts
@@ -1,5 +1,3 @@
-import { composeConfigs } from 'storybook/internal/preview-api';
-
 import actionsAddon from '@storybook/addon-actions';
 import backgroundsAddon from '@storybook/addon-backgrounds';
 // We can't use docs as function yet because of the --test flag. Once we figure out disabling docs properly in CSF4, we can change this
@@ -9,6 +7,8 @@ import highlightAddon from '@storybook/addon-highlight';
 import measureAddon from '@storybook/addon-measure';
 import outlineAddon from '@storybook/addon-outline';
 import viewportAddon from '@storybook/addon-viewport';
+
+import { composeConfigs } from 'storybook/preview-api';
 
 export default composeConfigs([
   actionsAddon(),

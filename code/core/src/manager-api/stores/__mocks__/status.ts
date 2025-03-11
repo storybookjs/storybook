6863fa68d55b0afb72d76c4f9d33ec39c5ec5092
Jeppe Reinhold <jeppe@reinhold.is>
2025-03-11 14:08:25 +0100

    make tests work in node

--- code/core/src/manager-api/stores/__mocks__/status.ts
+++ code/core/src/manager-api/stores/__mocks__/status.ts
@@ -1,15 +1,10 @@
-import * as testUtils from 'storybook/test';
-
 import { createStatusStore } from '../../../shared/status-store';
 import { UNIVERSAL_STATUS_STORE_OPTIONS } from '../../../shared/status-store';
 import { useUniversalStore } from '../../../shared/universal-store/use-universal-store-manager';
 import { experimental_MockUniversalStore } from '../../root';
 
 const mockStatusStore = createStatusStore({
-  universalStatusStore: new experimental_MockUniversalStore(
-    UNIVERSAL_STATUS_STORE_OPTIONS,
-    testUtils
-  ),
+  universalStatusStore: new experimental_MockUniversalStore(UNIVERSAL_STATUS_STORE_OPTIONS),
   useUniversalStore,
   environment: 'manager',
 });

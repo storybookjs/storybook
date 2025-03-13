bd0ffbfc24403f1b2e8c32867f819099032b0ce3
Jeppe Reinhold <jeppe@reinhold.is>
2025-03-13 19:53:46 +0100

    add comment explaining why we don't create status store in vitest subprocess.

--- code/core/src/core-server/stores/status.ts
+++ code/core/src/core-server/stores/status.ts
@@ -4,6 +4,13 @@ import { UniversalStore } from '../../shared/universal-store';
 
 const statusStore = createStatusStore({
   universalStatusStore:
+    /*
+    This is a temporary workaround, to ensure that the store is not created in the
+    vitest sub-process in addon-test, even though it imports from core-server
+    If it was created in the sub-process, it would try to connect to the leader in the dev server
+    before it was ready.
+    This will be fixed when we do the planned UniversalStore v0.2.
+    */
     process.env.VITEST !== 'true'
       ? UniversalStore.create({
           ...UNIVERSAL_STATUS_STORE_OPTIONS,

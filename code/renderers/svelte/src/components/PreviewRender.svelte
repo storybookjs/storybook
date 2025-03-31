96dffb536ee9e2a55a667d170239ae7b730e77f9
Jeppe Reinhold <jeppe@reinhold.is>
2025-03-31 14:15:56 +0200

    migrate internal Svelte components to v5

--- code/renderers/svelte/src/components/PreviewRender.svelte
+++ code/renderers/svelte/src/components/PreviewRender.svelte
@@ -1,51 +1,28 @@
 <script>
-  import SlotDecorator from './SlotDecorator.svelte';
+  import DecoratorHandler from './DecoratorHandler.svelte';
   import { dedent } from 'ts-dedent';
 
-  export let name;
-  export let title;
-  export let storyFn;
-  export let showError;
+  const { name, title, storyFn, showError } = $props();
 
   let {
-    /** @type {SvelteComponent} */
+    /** @type {import('svelte').SvelteComponent} */
     Component,
-    /** @type {any} */
     props = {},
-    /** @type {any} whether this level of the decorator chain is the last, ie. the actual story */
     argTypes,
-  } = storyFn();
-  let firstTime = true;
+  } = $derived.by(storyFn);
 
-  // the first time we don't want to call storyFn two times so we just return the values
-  // we already have from the previous call. If storyFn changes this function will run
-  // again but this time firstTime will be false
-  function getStoryFnValue(storyFn) {
-    if (firstTime) {
-      firstTime = false;
-      return {
-        Component,
-        props,
-        on,
-        argTypes,
-      };
-    }
-    return storyFn();
-  }
-  // reactive, re-render on storyFn change
-  $: ({ Component, props = {}, on, argTypes } = getStoryFnValue(storyFn));
-
-  // set the argTypes context, read by the last SlotDecorator that renders the original story
-  if (!Component) {
-    showError({
-      title: `Expecting a Svelte component from the story: "${name}" of "${title}".`,
-      description: dedent`
+  $effect(() => {
+    if (!Component) {
+      showError({
+        title: `Expecting a Svelte component from the story: "${name}" of "${title}".`,
+        description: dedent`
         Did you forget to return the Svelte component configuration from the story?
         Use "() => ({ Component: YourComponent, props: {} })"
         when defining the story.
       `,
-    });
-  }
+      });
+    }
+  });
 </script>
 
-<SlotDecorator {Component} {props} {argTypes} />
+<DecoratorHandler {Component} {props} {argTypes} />

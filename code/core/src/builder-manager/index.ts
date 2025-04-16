ad524a00a95e9da55aace08ce065d04675e75b41
Jeppe Reinhold <jeppe@reinhold.is>
2025-04-16 23:06:09 +0200

    update browser version data

--- code/core/src/builder-manager/index.ts
+++ code/core/src/builder-manager/index.ts
@@ -28,7 +28,14 @@ const isRootPath = /^\/($|\?)/;
 let compilation: Compilation;
 let asyncIterator: ReturnType<StarterFunction> | ReturnType<BuilderFunction>;
 
-export const BROWSER_TARGETS = ['chrome130', 'safari18.1', 'edge132', 'firefox134'];
+export const BROWSER_TARGETS = [
+  'chrome131',
+  'edge134',
+  'firefox136',
+  'safari18.3',
+  'ios18.3',
+  'opera117',
+];
 
 export const getConfig: ManagerBuilder['getConfig'] = async (options) => {
   const [addonsEntryPoints, customManagerEntryPoint, tsconfigPath, envs] = await Promise.all([

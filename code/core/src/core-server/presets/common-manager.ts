5750c694a82dbf66639749300bd711144e962079
Yann Braga <yannbf@gmail.com>
2025-04-18 13:49:00 +0200

    fix addon ordering

--- code/core/src/core-server/presets/common-manager.ts
+++ code/core/src/core-server/presets/common-manager.ts
@@ -3,13 +3,16 @@ import { global } from '@storybook/global';
 
 import { addons } from 'storybook/manager-api';
 
-import actionsManager from '../../actions/manager';
-import backgroundsManager from '../../backgrounds/manager';
-import componentTestingManager from '../../component-testing/manager';
+/* eslint-disable prettier/prettier */
+// THE ORDER OF THESE IMPORTS MATTERS! IT DEFINES THE ORDER OF PANELS AND TOOLS!
 import controlsManager from '../../controls/manager';
+import actionsManager from '../../actions/manager';
+import componentTestingManager from '../../component-testing/manager';
+import backgroundsManager from '../../backgrounds/manager';
 import measureManager from '../../measure/manager';
 import outlineManager from '../../outline/manager';
 import viewportManager from '../../viewport/manager';
+/* eslint-enable prettier/prettier */
 
 const TAG_FILTERS = 'tag-filters';
 const STATIC_FILTER = 'static-filter';

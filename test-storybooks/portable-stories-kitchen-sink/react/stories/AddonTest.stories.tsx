a38c67276b813188be66521e0e2b07b39a565924
Jeppe Reinhold <jeppe@reinhold.is>
2025-04-22 09:25:47 +0200

    fix react test storybook

--- test-storybooks/portable-stories-kitchen-sink/react/stories/AddonTest.stories.tsx
+++ test-storybooks/portable-stories-kitchen-sink/react/stories/AddonTest.stories.tsx
@@ -23,7 +23,7 @@ const { pass } = instrument({
 
 export const ExpectedFailure: Story = {
   args: {
-    forceFailure: true,
+    forceFailure: false,
   },
   play: async ({ args }) => {
     await pass();

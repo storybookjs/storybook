329c7bd4d390f3fbf3c5f4bd5a19170ad53899db
Yann Braga <yannbf@gmail.com>
2025-04-22 10:22:30 +0200

    fix

--- code/lib/cli-storybook/src/sandbox.ts
+++ code/lib/cli-storybook/src/sandbox.ts
@@ -227,7 +227,6 @@ export const sandbox = async ({
         // we run doInitiate, instead of initiate, to avoid sending this init event to telemetry, because it's not a real world project
         // @ts-expect-error (no types for this)
         const { initiate } = await import('create-storybook');
-        // @ts-expect-error (no types for this)
         await initiate({
           dev: process.env.CI !== 'true' && process.env.IN_STORYBOOK_SANDBOX !== 'true',
           ...options,

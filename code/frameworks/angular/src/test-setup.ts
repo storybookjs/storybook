9de1b1d295cbbebf2e9094021f693b70080beef4
Matthieu Riegler <kyro38@gmail.com>
2025-04-28 21:25:28 +0200

    chore: remove dependency on `@angular/platform-browser-dynamic`
    Storybook can rely on `@angular/platform-browser` directly.

--- code/frameworks/angular/src/test-setup.ts
+++ code/frameworks/angular/src/test-setup.ts
@@ -1,8 +1,5 @@
 import '@analogjs/vite-plugin-angular/setup-vitest';
 import { getTestBed } from '@angular/core/testing';
-import {
-  BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting,
-} from '@angular/platform-browser-dynamic/testing';
+import { BrowserTestingModule, platformBrowserTesting } from '@angular/platform-browser/testing';
 
-getTestBed().initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting());
+getTestBed().initTestEnvironment(BrowserTestingModule, platformBrowserTesting());

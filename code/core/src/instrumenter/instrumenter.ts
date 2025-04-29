3e3eac2e5a43f542c92828791e2f73f88c9d5e8d
Norbert de Langen <ndelangen@me.com>
2025-04-29 15:22:37 +0200

    Discard changes to code/core/src/instrumenter/instrumenter.ts

--- code/core/src/instrumenter/instrumenter.ts
+++ code/core/src/instrumenter/instrumenter.ts
@@ -11,7 +11,6 @@ import { global } from '@storybook/global';
 
 import { processError } from '@vitest/utils/error';
 
-import { isModule, isObject } from '../shared/type-guards/type-guards';
 import { EVENTS } from './EVENTS';
 import { addons } from './preview-api';
 import type { Call, CallRef, ControlStates, LogItem, Options, State, SyncPayload } from './types';
@@ -34,6 +33,10 @@ const alreadyCompletedException = new Error(
   `This function ran after the play function completed. Did you forget to \`await\` it?`
 );
 
+const isObject = (o: unknown): o is object =>
+  Object.prototype.toString.call(o) === '[object Object]';
+const isModule = (o: unknown): o is NodeModule =>
+  Object.prototype.toString.call(o) === '[object Module]';
 const isInstrumentable = (o: unknown) => {
   if (!isObject(o) && !isModule(o)) {
     return false;

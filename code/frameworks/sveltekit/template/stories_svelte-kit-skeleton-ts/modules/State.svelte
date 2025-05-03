f6aba2b4a568cb395155edf16627cb7499e980b9
Mateusz Kadlubowski <xeho91@pm.me>
2025-05-03 09:36:59 +0800

    add testing stories file for SveltKit `$app/state`

---
+++ code/frameworks/sveltekit/template/stories_svelte-kit-skeleton-ts/modules/State.svelte
@@ -0,0 +1,10 @@
+<script>
+	import { page, navigating, updated } from '$app/state';
+
+	updated.check();
+</script>
+
+<p>Directly importing</p>
+<pre>{JSON.stringify($page, null, 2)}</pre>
+<pre>{JSON.stringify($navigating, null, 2)}</pre>
+<pre>{JSON.stringify($updated, null, 2)}</pre>
---
+++ code/frameworks/sveltekit/template/stories_svelte-kit-skeleton-ts/modules/state.stories.js
@@ -0,0 +1,101 @@
+import State from './State.svelte';
+
+export default {
+  title: 'stories/frameworks/sveltekit/modules/state',
+  component: State,
+};
+
+export const AllUndefined = {};
+
+export const Page = {
+  parameters: {
+    sveltekit_experimental: {
+      page: {
+        data: {
+          test: 'passed',
+        },
+      },
+    },
+  },
+};
+
+export const Navigating = {
+  parameters: {
+    sveltekit_experimental: {
+      navigating: {
+        route: {
+          id: '/storybook',
+        },
+      },
+    },
+  },
+};
+
+export const Updated = {
+  parameters: {
+    sveltekit_experimental: {
+      updated: true,
+    },
+  },
+};
+
+export const PageAndNavigating = {
+  parameters: {
+    sveltekit_experimental: {
+      page: {
+        data: {
+          test: 'passed',
+        },
+      },
+      navigating: {
+        route: {
+          id: '/storybook',
+        },
+      },
+    },
+  },
+};
+
+export const PageAndUpdated = {
+  parameters: {
+    sveltekit_experimental: {
+      page: {
+        data: {
+          test: 'passed',
+        },
+      },
+      updated: true,
+    },
+  },
+};
+
+export const NavigatingAndUpdated = {
+  parameters: {
+    sveltekit_experimental: {
+      navigating: {
+        route: {
+          id: '/storybook',
+        },
+      },
+      updated: true,
+    },
+  },
+};
+
+export const AllThree = {
+  parameters: {
+    sveltekit_experimental: {
+      page: {
+        data: {
+          test: 'passed',
+        },
+      },
+      navigating: {
+        route: {
+          id: '/storybook',
+        },
+      },
+      updated: true,
+    },
+  },
+};

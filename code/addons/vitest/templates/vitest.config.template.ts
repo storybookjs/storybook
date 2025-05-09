51503d44a5a69376695bf71a9745a975fda3b03c
Kyle Gach <kyle.gach@gmail.com>
2025-05-09 10:08:20 -0600

    Fix missed docs link

--- code/addons/vitest/templates/vitest.config.template.ts
+++ code/addons/vitest/templates/vitest.config.template.ts
@@ -8,7 +8,7 @@ import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
 const dirname =
   typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
 
-// More info at: https://storybook.js.org/docs/writing-tests/vitest-addon
+// More info at: https://storybook.js.org/docs/next/writing-tests/vitest-addon
 export default defineConfig({
   test: {
     workspace: [

c06bce6cd079c51b417666baf096a9e1767a4469
Valentin Palkovic <valentin@chromatic.com>
2025-05-09 14:50:58 +0200

    Introduce experimental_afterEach wrapper for testing purposes

--- code/frameworks/angular/src/client/config.ts
+++ code/frameworks/angular/src/client/config.ts
@@ -4,3 +4,7 @@ export { render, renderToCanvas } from './render';
 export { decorateStory as applyDecorators } from './decorateStory';
 
 export const parameters = { renderer: 'angular' as const };
+
+export const experimental_afterEach = () => {
+  console.log('afterEach');
+};

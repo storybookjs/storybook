64be12f05ee24d498975df0709e4a894c82e1b19
Kasper Peulen <kasperpeulen@gmail.com>
2025-05-10 12:23:50 +0200

    Merge pull request #31435 from storybookjs/kasper/fix-args-mutation
    Test: Make sure that lit arrays are not cloned
    (cherry picked from commit 6f91cee5640075188cbb05b1e4a1aab5f7804c64)

--- code/lib/test/src/index.ts
+++ code/lib/test/src/index.ts
@@ -97,7 +97,11 @@ export const traverseArgs = (value: unknown, depth = 0, key?: string): unknown =
 
   if (Array.isArray(value)) {
     depth++;
-    return value.map((item) => traverseArgs(item, depth));
+    // we loop instead of map to prevent this lit issue:
+    // https://github.com/storybookjs/storybook/issues/25651
+    for (let i = 0; i < value.length; i++) {
+      value[i] = traverseArgs(value[i], depth);
+    }
   }
 
   if (typeof value === 'object' && value.constructor === Object) {

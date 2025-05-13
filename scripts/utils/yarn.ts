3076499864f54bcebbebcbf6e2bef12f4ea06497
Yann Braga <yannbf@gmail.com>
2025-05-13 16:23:45 +0200

    fix

--- scripts/utils/yarn.ts
+++ scripts/utils/yarn.ts
@@ -31,9 +31,9 @@ export const addPackageResolutions = async ({ cwd, dryRun }: YarnOptions) => {
     ...storybookVersions,
     // this is for our CI test, ensure we use the same version as docker image, it should match version specified in `./code/package.json` and `.circleci/config.yml`
     '@swc/core': '1.5.7',
-    playwright: '1.48.1',
-    'playwright-core': '1.48.1',
-    '@playwright/test': '1.48.1',
+    playwright: '1.52.0',
+    'playwright-core': '1.52.0',
+    '@playwright/test': '1.52.0',
   };
   await writeJSON(packageJsonPath, packageJson, { spaces: 2 });
 };

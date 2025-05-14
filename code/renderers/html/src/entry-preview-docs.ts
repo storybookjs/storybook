50e1c8d05220313077410246d13139478abea5e3
Jeppe Reinhold <jeppe@reinhold.is>
2025-05-14 14:58:08 +0200

    html: always enhance argTypes

--- code/renderers/html/src/entry-preview-docs.ts
+++ code/renderers/html/src/entry-preview-docs.ts
@@ -1,5 +1,5 @@
-import { SourceType, enhanceArgTypes } from 'storybook/internal/docs-tools';
-import type { ArgTypesEnhancer, DecoratorFunction } from 'storybook/internal/types';
+import { SourceType } from 'storybook/internal/docs-tools';
+import type { DecoratorFunction } from 'storybook/internal/types';
 
 import { sourceDecorator } from './docs/sourceDecorator';
 import type { HtmlRenderer } from './types';
@@ -17,5 +17,3 @@ export const parameters = {
     },
   },
 };
-
-export const argTypesEnhancers: ArgTypesEnhancer[] = [enhanceArgTypes];

5aff0a7c1be3cb016662a83448ecea4270706e73
Gert Hengeveld <info@ghengeveld.nl>
2025-05-14 13:25:36 +0200

    Remove nonexistent export

--- code/core/src/highlight/index.ts
+++ code/core/src/highlight/index.ts
@@ -1,7 +1,6 @@
 export { HIGHLIGHT, REMOVE_HIGHLIGHT, RESET_HIGHLIGHT, SCROLL_INTO_VIEW } from './constants';
 export type {
   ClickEventDetails,
-  HighlightMenu,
   HighlightMenuItem,
   HighlightOptions,
   HighlightParameters,

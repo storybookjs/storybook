1983ab0c1a86f83aaf8951e6cdc60633b22d5c07
Norbert de Langen <ndelangen@me.com>
2025-05-15 12:44:12 +0200

    split out the serve into a background task, maybe this will yield more info
    also reduce number of workers to 1

--- scripts/tasks/test-runner-build.ts
+++ scripts/tasks/test-runner-build.ts
@@ -15,7 +15,7 @@ export const testRunnerBuild: Task & { port: number } = {
     const flags = [
       `--url http://127.0.0.1:${this.port}`,
       '--junit',
-      '--maxWorkers=2',
+      '--maxWorkers=1',
       '--failOnConsole',
       '--index-json',
     ];

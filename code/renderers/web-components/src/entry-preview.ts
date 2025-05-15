85176f8e5eb43bc7e9349d1e524721812e278abc
Jeppe Reinhold <jeppe@reinhold.is>
2025-05-15 13:30:01 +0200

    wc: always extract argTypes, even without parameters.docs

--- code/renderers/web-components/src/entry-preview.ts
+++ code/renderers/web-components/src/entry-preview.ts
@@ -1,2 +1,21 @@
-export const parameters: {} = { renderer: 'web-components' };
+import { enhanceArgTypes } from 'storybook/internal/docs-tools';
+import type { ArgTypesEnhancer, DecoratorFunction } from 'storybook/internal/types';
+
+import { extractArgTypes, extractComponentDescription } from './docs/custom-elements';
+import { sourceDecorator } from './docs/sourceDecorator';
+import type { WebComponentsRenderer } from './types';
+
 export { render, renderToCanvas } from './render';
+
+export const decorators: DecoratorFunction<WebComponentsRenderer>[] = [sourceDecorator];
+
+export const parameters = {
+  renderer: 'web-components',
+  docs: {
+    extractArgTypes,
+    extractComponentDescription,
+    story: { inline: true },
+  },
+};
+
+export const argTypesEnhancers: ArgTypesEnhancer<WebComponentsRenderer>[] = [enhanceArgTypes];

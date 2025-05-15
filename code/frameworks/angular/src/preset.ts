b37224282b1e618b331cd8e4414e8c2a81f022a3
Jeppe Reinhold <jeppe@reinhold.is>
2025-05-15 14:18:36 +0200

    angular: always extract argTypes, even without parameters.docs

--- code/frameworks/angular/src/preset.ts
+++ code/frameworks/angular/src/preset.ts
@@ -10,16 +10,23 @@ const getAbsolutePath = <I extends string>(input: I): I =>
 export const addons: PresetProperty<'addons'> = [
   require.resolve('./server/framework-preset-angular-cli'),
   require.resolve('./server/framework-preset-angular-ivy'),
-  require.resolve('./server/framework-preset-angular-docs'),
 ];
 
-export const previewAnnotations: PresetProperty<'previewAnnotations'> = (entries = [], options) => {
+export const previewAnnotations: PresetProperty<'previewAnnotations'> = async (
+  entries = [],
+  options
+) => {
   const annotations = [...entries, require.resolve('./client/config')];
 
   if ((options as any as StandaloneOptions).enableProdMode) {
     annotations.unshift(require.resolve('./client/preview-prod'));
   }
 
+  const docsConfig = await options.presets.apply('docs', {}, options);
+  const docsEnabled = Object.keys(docsConfig).length > 0;
+  if (docsEnabled) {
+    annotations.push(require.resolve('./client/docs/config'));
+  }
   return annotations;
 };
 

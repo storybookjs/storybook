26f86665a16974cf2dbd6e3799db605cc496c265
Michael Shilman <shilman@users.noreply.github.com>
2025-05-16 08:51:20 +0800

    Merge pull request #31484 from storybookjs/dannyhw/fix-jsx-automatic
    React Native Web: Fix expo router by setting JSX to automatic
    
    (cherry picked from commit 50b17fa7e5c355e8535ed35b65ccdccc0d7250dd)

--- code/frameworks/react-native-web-vite/src/preset.ts
+++ code/frameworks/react-native-web-vite/src/preset.ts
@@ -25,7 +25,7 @@ export function reactNativeWeb(): PluginOption {
         optimizeDeps: {
           include: [],
           esbuildOptions: {
-            jsx: 'transform',
+            jsx: 'automatic',
             resolveExtensions: [
               '.web.js',
               '.web.ts',

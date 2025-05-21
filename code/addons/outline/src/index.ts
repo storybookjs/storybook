8050a06b5ee70f4c25463162a829b49fbfc0964e
Kasper Peulen <kasperpeulen@gmail.com>
2025-05-21 12:57:39 +0200

    Don't export in addon-outline

--- code/addons/outline/src/index.ts
+++ code/addons/outline/src/index.ts
@@ -1,9 +1,1 @@
-import { definePreviewAddon } from 'storybook/internal/csf';
-import { definePreview } from 'storybook/internal/preview-api';
-
-import * as addonAnnotations from './preview';
-import type { OutlineTypes } from './types';
-
-export type { OutlineTypes };
-
-export default () => definePreviewAddon<OutlineTypes>(addonAnnotations);
+import './error';

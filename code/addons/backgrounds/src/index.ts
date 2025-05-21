f7a85a7c78d0e1db892dbbc4c56622c85f35722c
Kasper Peulen <kasperpeulen@gmail.com>
2025-05-21 12:58:08 +0200

    Don't export in addon-backgrounds

--- code/addons/backgrounds/src/index.ts
+++ code/addons/backgrounds/src/index.ts
@@ -1,9 +1,1 @@
-import { definePreviewAddon } from 'storybook/internal/csf';
-import { definePreview } from 'storybook/internal/preview-api';
-
-import * as addonAnnotations from './preview';
-import type { BackgroundTypes } from './types';
-
-export default () => definePreviewAddon<BackgroundTypes>(addonAnnotations);
-
-export type { BackgroundTypes };
+import './error';

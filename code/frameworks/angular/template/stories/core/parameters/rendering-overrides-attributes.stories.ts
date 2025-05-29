a084f95251787cdd889faf686077c75e1e3b02ab
christophrogalla <christophrogalla@web.de>
2025-05-29 09:34:18 +0200

    [Angular] feat: add rendering bug as test story

---
+++ code/frameworks/angular/template/stories/core/parameters/rendering-overrides-attributes.stories.ts
@@ -0,0 +1,34 @@
+import { Meta, StoryObj } from '@storybook/angular';
+import RenderingBugComponent from './rendering-bug-component/rendering-bug-component';
+
+const meta: Meta<RenderingBugComponent> = {
+  component: RenderingBugComponent,
+  tags: ['autodocs'],
+  parameters: {
+    useTestBedRenderer: true,
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<RenderingBugComponent>;
+
+export const RenderFuncOerridesAttributes: Story = {
+  args: {
+    label: 'Just a Test',
+  },
+  render: (args) => {
+    const sample123 = (someArgument: string) => {
+      console.log('Overridden', someArgument);
+    };
+    return {
+      template: `
+        <div>
+          <span>Check Logs</span>
+          <rendering-bug-component [label]="label"></rendering-bug-component>
+        </div>`,
+      props: { ...args, sample123 },
+      component: RenderingBugComponent,
+    };
+  },
+};

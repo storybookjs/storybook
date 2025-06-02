dcb271d0bd82e3bef3aacb07b25a82366c4a365a
Kasper Peulen <kasperpeulen@gmail.com>
2025-06-02 22:16:37 +0200

    Fix a11y type

--- code/addons/a11y/src/types.ts
+++ code/addons/a11y/src/types.ts
@@ -19,7 +19,7 @@ export interface A11yGlobals {
    *
    * @see https://storybook.js.org/docs/writing-tests/accessibility-testing
    */
-  a11y: {
+  a11y?: {
     /**
      * Prevent the addon from executing automated accessibility checks upon visiting a story. You
      * can still trigger the checks from the addon panel.

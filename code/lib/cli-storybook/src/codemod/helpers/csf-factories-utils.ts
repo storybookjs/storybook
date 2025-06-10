2c1afc54c41840ed7e579e0f12c91eed720e4d44
Kasper Peulen <kasperpeulen@gmail.com>
2025-06-10 13:51:12 +0200

    Address feedback

--- code/lib/cli-storybook/src/codemod/helpers/csf-factories-utils.ts
+++ code/lib/cli-storybook/src/codemod/helpers/csf-factories-utils.ts
@@ -1,5 +1,25 @@
 import { types as t, traverse } from 'storybook/internal/babel';
 
+const projectAnnotationNames = [
+  'decorators',
+  'parameters',
+  'args',
+  'argTypes',
+  'loaders',
+  'beforeEach',
+  'afterEach',
+  'render',
+  'tags',
+  'mount',
+  'argsEnhancers',
+  'argTypesEnhancers',
+  'beforeAll',
+  'initialGlobals',
+  'globalTypes',
+  'applyDecorators',
+  'runStep',
+];
+
 export function cleanupTypeImports(programNode: t.Program, disallowList: string[]) {
   const usedIdentifiers = new Set<string>();
 
@@ -83,28 +103,7 @@ export function getConfigProperties(
   // Collect properties from named exports
   for (const [name, decl] of Object.entries(exportDecls)) {
     // only include real preview exports to definePreview factory
-    if (
-      options.configType === 'preview' &&
-      ![
-        'decorators',
-        'parameters',
-        'args',
-        'argTypes',
-        'loaders',
-        'beforeEach',
-        'afterEach',
-        'render',
-        'tags',
-        'mount',
-        'argsEnhancers',
-        'argTypesEnhancers',
-        'beforeAll',
-        'initialGlobals',
-        'globalTypes',
-        'applyDecorators',
-        'runStep',
-      ].includes(name)
-    ) {
+    if (options.configType === 'preview' && !projectAnnotationNames.includes(name)) {
       continue;
     }
     if (t.isVariableDeclarator(decl) && decl.init) {

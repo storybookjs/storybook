2c5e9157fa83e2f21f48d72b6f35543fd16fbb02
Norbert de Langen <ndelangen@me.com>
2025-06-18 18:06:20 +0200

    Fix typo in extra development dependencies in sandbox-parts.ts
    - Corrected the package name from `storybook/test-runner` to `@storybook/test-runner` in `sandbox-parts.ts` to ensure proper dependency resolution.

--- scripts/tasks/sandbox-parts.ts
+++ scripts/tasks/sandbox-parts.ts
@@ -489,7 +489,7 @@ export async function addExtraDependencies({
   debug: boolean;
   extraDeps?: string[];
 }) {
-  const extraDevDeps = ['storybook/test-runner@0.23.1--canary.fd3c654.0'];
+  const extraDevDeps = ['@storybook/test-runner@0.23.1--canary.fd3c654.0'];
 
   if (debug) {
     logger.log('\uD83C\uDF81 Adding extra dev deps', extraDevDeps);

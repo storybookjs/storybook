60e248010163a8ef4dc6cde4e04091b6b3cacb47
Norbert de Langen <ndelangen@me.com>
2025-06-19 11:03:17 +0200

    Update extra development dependency version in sandbox-parts.ts
    - Updated the version of `@storybook/test-runner` in `sandbox-parts.ts` from `0.23.1--canary.49024ff.0` to `0.23.1--canary.de6531a.0` to incorporate the latest updates.

--- scripts/tasks/sandbox-parts.ts
+++ scripts/tasks/sandbox-parts.ts
@@ -489,7 +489,7 @@ export async function addExtraDependencies({
   debug: boolean;
   extraDeps?: string[];
 }) {
-  const extraDevDeps = ['@storybook/test-runner@0.23.1--canary.49024ff.0'];
+  const extraDevDeps = ['@storybook/test-runner@0.23.1--canary.de6531a.0'];
 
   if (debug) {
     logger.log('\uD83C\uDF81 Adding extra dev deps', extraDevDeps);

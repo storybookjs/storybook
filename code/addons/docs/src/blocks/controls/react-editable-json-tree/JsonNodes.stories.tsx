42152881dbcb812f89848d70a86b3a1903e97894
Gert Hengeveld <info@ghengeveld.nl>
2025-06-25 17:12:31 +0200

    Update story file

--- code/addons/docs/src/blocks/controls/react-editable-json-tree/JsonNodes.stories.tsx
+++ code/addons/docs/src/blocks/controls/react-editable-json-tree/JsonNodes.stories.tsx
@@ -11,14 +11,18 @@ const meta = {
     value: { any: 'value' },
     function: { value: () => {} },
   },
+  parameters: {
+    // This story exists only to verify proper behavior on its docs page, the snapshot is irrelevant
+    chromatic: { disableSnapshot: true },
+  },
 };
 
 export default meta;
 
-export const JsonNodes = () => {
-  return (
+export const JsonNodes = {
+  render: () => (
     <a href="https://www.google.com/" target="_blank" rel="noreferrer">
       Confirm the link works by pressing enter key
     </a>
-  );
+  ),
 };

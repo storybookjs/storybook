721866f8cb2242e21e0ca211ed84d56068152fe9
Norbert de Langen <ndelangen@me.com>
2025-07-02 18:34:56 +0200

    Refactor postinstallAddon to improve module resolution and error handling for dynamic imports

--- code/lib/cli-storybook/src/postinstallAddon.ts
+++ code/lib/cli-storybook/src/postinstallAddon.ts
@@ -1,41 +1,42 @@
 import { createRequire } from 'node:module';
+import { fileURLToPath } from 'node:url';
 
 import { importModule } from '../../../core/src/shared/utils/module';
 import type { PostinstallOptions } from './add';
 
+const require = createRequire(import.meta.url);
 export const postinstallAddon = async (addonName: string, options: PostinstallOptions) => {
   const hookPath = `${addonName}/postinstall`;
+  let modulePath: string;
   try {
-    const modulePath = import.meta.resolve(hookPath, process.cwd());
-    const loaded = await importModule(modulePath);
-
-    const postinstall = loaded?.default || loaded?.postinstall || loaded;
-
-    if (!postinstall || typeof postinstall !== 'function') {
-      return;
-    }
-
-    try {
-      console.log(`Running postinstall script for ${addonName}`);
-      await postinstall(options);
-    } catch (e) {
-      console.error(`Error running postinstall script for ${addonName}`);
-      console.error(e);
-    }
+    modulePath = import.meta.resolve(hookPath, process.cwd()) || require.resolve(hookPath);
   } catch (e) {
-    const loaded = createRequire(import.meta.url)(hookPath);
-    const postinstall = loaded?.default || loaded?.postinstall || loaded;
+    return;
+  }
 
-    if (!postinstall || typeof postinstall !== 'function') {
-      return;
-    }
+  let moduledLoaded: any;
 
-    try {
-      console.log(`Running postinstall script for ${addonName}`);
-      await postinstall(options);
-    } catch (e) {
-      console.error(`Error running postinstall script for ${addonName}`);
-      console.error(e);
-    }
+  try {
+    moduledLoaded = await import(hookPath)
+      .catch(() => importModule(hookPath))
+      .catch(() => importModule(modulePath))
+      .catch(() => importModule(fileURLToPath(modulePath)))
+      .catch(() => require(fileURLToPath(modulePath)));
+  } catch (e) {
+    return;
+  }
+
+  const postinstall = moduledLoaded?.default || moduledLoaded?.postinstall || moduledLoaded;
+
+  if (!postinstall || typeof postinstall !== 'function') {
+    return;
+  }
+
+  try {
+    console.log(`Running postinstall script for ${addonName}`);
+    await postinstall(options);
+  } catch (e) {
+    console.error(`Error running postinstall script for ${addonName}`);
+    console.error(e);
   }
 };

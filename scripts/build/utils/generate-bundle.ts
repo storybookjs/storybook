42ebcaf9792006232a65979191b75a83fa46e4c4
Jeppe Reinhold <jeppe@reinhold.is>
2025-07-02 12:27:17 +0200

    enable code splitting, undo create-storybook types

--- scripts/build/utils/generate-bundle.ts
+++ scripts/build/utils/generate-bundle.ts
@@ -69,6 +69,7 @@ export async function generateBundle(
     const sharedOptions = {
       format: 'esm',
       bundle: true,
+      splitting: true,
       metafile: true,
       minifyIdentifiers: isProduction,
       minifySyntax: isProduction,

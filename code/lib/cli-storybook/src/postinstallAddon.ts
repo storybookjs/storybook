e76bd3e52944a5d3409f438490ade51257ae721b
Norbert de Langen <ndelangen@me.com>
2025-07-02 18:48:48 +0200

    Improve error logging in postinstallAddon by adding a console message for missing postinstall function

--- code/lib/cli-storybook/src/postinstallAddon.ts
+++ code/lib/cli-storybook/src/postinstallAddon.ts
@@ -1,7 +1,9 @@
 import { createRequire } from 'node:module';
 import { fileURLToPath } from 'node:url';
 
-import { importModule } from '../../../core/src/shared/utils/module';
+import { join } from 'pathe';
+
+import { importModule, resolvePackageDir } from '../../../core/src/shared/utils/module';
 import type { PostinstallOptions } from './add';
 
 const require = createRequire(import.meta.url);
@@ -11,10 +13,12 @@ export const postinstallAddon = async (addonName: string, options: PostinstallOp
   try {
     modulePath = import.meta.resolve(hookPath, process.cwd()) || require.resolve(hookPath);
   } catch (e) {
-    return;
+    modulePath = join(resolvePackageDir(addonName), 'postinstall');
   }
 
   let moduledLoaded: any;
+
+  console.log({ modulePath, hookPath, cwd: process.cwd(), addonName });
 
   try {
     moduledLoaded = await import(hookPath)
@@ -31,6 +35,7 @@ export const postinstallAddon = async (addonName: string, options: PostinstallOp
   const postinstall = moduledLoaded?.default || moduledLoaded?.postinstall || moduledLoaded;
 
   if (!postinstall || typeof postinstall !== 'function') {
+    console.log(`Error finding postinstall function for ${addonName}`);
     return;
   }
 

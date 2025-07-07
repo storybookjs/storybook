47ffb579ba632a99d774ead480efb67d0bb71add
Norbert de Langen <ndelangen@me.com>
2025-07-07 23:21:13 +0200

    convert renderer-react to ESM & new build script

--- scripts/build/entries/index.ts
+++ scripts/build/entries/index.ts
@@ -606,6 +606,51 @@ export const buildEntries = {
       ],
     },
   },
+  '@storybook/react': {
+    entries: {
+      browser: [
+        {
+          exportEntries: ['.'],
+          entryPoint: './src/index.ts',
+        },
+        {
+          exportEntries: ['./preview'],
+          entryPoint: './src/preview.tsx',
+        },
+        {
+          exportEntries: ['./entry-preview'],
+          entryPoint: './src/entry-preview.tsx',
+          dts: false,
+        },
+        {
+          exportEntries: ['./entry-preview-argtypes'],
+          entryPoint: './src/entry-preview-argtypes.ts',
+          dts: false,
+        },
+        {
+          exportEntries: ['./entry-preview-docs'],
+          entryPoint: './src/entry-preview-docs.ts',
+          dts: false,
+        },
+        {
+          exportEntries: ['./entry-preview-rsc'],
+          entryPoint: './src/entry-preview-rsc.tsx',
+          dts: false,
+        },
+        {
+          exportEntries: ['./experimental-playwright'],
+          entryPoint: './src/playwright.ts',
+        },
+      ],
+      node: [
+        {
+          exportEntries: ['./preset'],
+          entryPoint: './src/preset.ts',
+          dts: false,
+        },
+      ],
+    },
+  },
 } satisfies BuildEntriesByPackageName;
 
 export function isBuildEntries(key: string): key is keyof typeof buildEntries {

46ae3140a4a4d974f7e31c411bcdb41507f60ced
Norbert de Langen <ndelangen@me.com>
2025-07-07 23:40:53 +0200

    convert renderer-vue3 to ESM & new build script

--- scripts/build/entries/index.ts
+++ scripts/build/entries/index.ts
@@ -710,6 +710,37 @@ export const buildEntries = {
       './internal/createReactiveProps': './static/createReactiveProps.svelte.js',
     },
   },
+  '@storybook/vue3': {
+    entries: {
+      browser: [
+        {
+          exportEntries: ['.'],
+          entryPoint: './src/index.ts',
+        },
+        {
+          exportEntries: ['./entry-preview'],
+          entryPoint: './src/entry-preview.ts',
+          dts: false,
+        },
+        {
+          exportEntries: ['./entry-preview-docs'],
+          entryPoint: './src/entry-preview-docs.ts',
+          dts: false,
+        },
+        {
+          exportEntries: ['./experimental-playwright'],
+          entryPoint: './src/playwright.ts',
+        },
+      ],
+      node: [
+        {
+          exportEntries: ['./preset'],
+          entryPoint: './src/preset.ts',
+          dts: false,
+        },
+      ],
+    },
+  },
 } satisfies BuildEntriesByPackageName;
 
 export function isBuildEntries(key: string): key is keyof typeof buildEntries {

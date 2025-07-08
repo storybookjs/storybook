b117cf7c0f96bf33348afc3360f9efbd754cb15f
Norbert de Langen <ndelangen@me.com>
2025-07-08 15:28:37 +0200

    Fix condition in generatePackageJsonFile to check for undefined dts property directly

--- scripts/build/utils/generate-package-json.ts
+++ scripts/build/utils/generate-package-json.ts
@@ -22,7 +22,7 @@ export async function generatePackageJsonFile(cwd: string, data: BuildEntries) {
       const dtsPath = entry.entryPoint?.replace('src', 'dist').replace(/\.tsx?/, '.d.ts');
       const jsPath = entry.entryPoint?.replace('src', 'dist').replace(/\.tsx?/, '.js');
 
-      if (entry.entryPoint && entry.dts === undefined) {
+      if (entry.dts === undefined) {
         pkgJson.exports[exportEntry] = {
           types: dtsPath,
           default: jsPath,

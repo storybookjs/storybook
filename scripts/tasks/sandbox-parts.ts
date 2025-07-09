0d3b78ab1c200a0b642f746370e18dd3a3588f4e
Norbert de Langen <ndelangen@me.com>
2025-07-09 15:24:30 +0200

    Update extraDevDeps in sandbox-parts.ts to use the latest canary version of @storybook/test-runner

--- scripts/tasks/sandbox-parts.ts
+++ scripts/tasks/sandbox-parts.ts
@@ -490,7 +490,7 @@ export async function addExtraDependencies({
   debug: boolean;
   extraDeps?: string[];
 }) {
-  const extraDevDeps = ['@storybook/test-runner@0.23.1--canary.8609766.0'];
+  const extraDevDeps = ['@storybook/test-runner@0.23.1--canary.f3d1819.0'];
 
   if (debug) {
     logger.log('\uD83C\uDF81 Adding extra dev deps', extraDevDeps);

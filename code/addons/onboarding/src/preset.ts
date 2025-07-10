364e9594428364fceaf2e38e41321b6ff3ab8e6e
Jeppe Reinhold <jeppe@reinhold.is>
2025-07-10 12:49:48 +0200

    replace require.resolve in addon-onboarding with json import

--- code/addons/onboarding/src/preset.ts
+++ code/addons/onboarding/src/preset.ts
@@ -1,5 +1,3 @@
-import { readFileSync } from 'node:fs';
-
 import type { Channel } from 'storybook/internal/channels';
 import { telemetry } from 'storybook/internal/telemetry';
 import type { CoreConfig, Options } from 'storybook/internal/types';
@@ -15,21 +13,19 @@ type Event = {
 export const experimental_serverChannel = async (channel: Channel, options: Options) => {
   const { disableTelemetry } = await options.presets.apply<CoreConfig>('core', {});
 
-  if (!disableTelemetry) {
-    const packageJsonPath = require.resolve('@storybook/addon-onboarding/package.json');
-
-    const { version: addonVersion } = JSON.parse(
-      readFileSync(packageJsonPath, { encoding: 'utf-8' })
-    );
-
-    channel.on(STORYBOOK_ADDON_ONBOARDING_CHANNEL, ({ type, ...event }: Event) => {
-      if (type === 'telemetry') {
-        telemetry('addon-onboarding', { ...event, addonVersion });
-      } else if (type === 'survey') {
-        telemetry('onboarding-survey', { ...event, addonVersion });
-      }
-    });
+  if (disableTelemetry) {
+    return channel;
   }
 
-  return channel;
+  const { default: packageJson } = await import('@storybook/addon-onboarding/package.json', {
+    with: { type: 'json' },
+  });
+
+  channel.on(STORYBOOK_ADDON_ONBOARDING_CHANNEL, ({ type, ...event }: Event) => {
+    if (type === 'telemetry') {
+      telemetry('addon-onboarding', { ...event, addonVersion: packageJson.version });
+    } else if (type === 'survey') {
+      telemetry('onboarding-survey', { ...event, addonVersion: packageJson.version });
+    }
+  });
 };

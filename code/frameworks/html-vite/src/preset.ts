34074b9abf2f27780e57858fcbd3e13e630aadac
Norbert de Langen <ndelangen@me.com>
2025-07-10 14:59:13 +0200

    make it ESM

--- code/frameworks/html-vite/src/preset.ts
+++ code/frameworks/html-vite/src/preset.ts
@@ -1,6 +1,6 @@
 import type { PresetProperty } from 'storybook/internal/types';
 
 export const core: PresetProperty<'core'> = {
-  builder: require.resolve('@storybook/builder-vite'),
-  renderer: require.resolve('@storybook/html/preset'),
+  builder: import.meta.resolve('@storybook/builder-vite'),
+  renderer: import.meta.resolve('@storybook/html/preset'),
 };

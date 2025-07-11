83af8add6157b2d63d92d588716550cf967ede26
Norbert de Langen <ndelangen@me.com>
2025-07-11 15:03:59 +0200

    simplify

--- code/frameworks/nextjs/src/export-mocks/webpack.ts
+++ code/frameworks/nextjs/src/export-mocks/webpack.ts
@@ -3,16 +3,16 @@ import type { Configuration as WebpackConfig } from 'webpack';
 import { getCompatibilityAliases } from '../compatibility/compatibility-map';
 
 const mapping = {
-  'next/headers': import.meta.resolve('@storybook/nextjs/headers.mock'),
-  'next/navigation': import.meta.resolve('@storybook/nextjs/navigation.mock'),
-  'next/router': import.meta.resolve('@storybook/nextjs/router.mock'),
-  'next/cache': import.meta.resolve('@storybook/nextjs/cache.mock'),
+  'next/headers': '@storybook/nextjs/headers.mock',
+  'next/navigation': '@storybook/nextjs/navigation.mock',
+  'next/router': '@storybook/nextjs/router.mock',
+  'next/cache': '@storybook/nextjs/cache.mock',
   ...getCompatibilityAliases(),
 };
 
 // Utility that assists in adding aliases to the Webpack configuration
 // and also doubles as alias solution for portable stories in Jest/Vitest/etc.
-export const getPackageAliases = ({ useESM = false }: { useESM?: boolean } = {}) => {
+export const getPackageAliases = () => {
   const aliases = Object.fromEntries(
     Object.entries(mapping).map(([originalPath, aliasedPath]) => [originalPath, aliasedPath])
   );
@@ -25,6 +25,6 @@ export const configureNextExportMocks = (baseConfig: WebpackConfig): void => {
 
   resolve.alias = {
     ...resolve.alias,
-    ...getPackageAliases({ useESM: true }),
+    ...getPackageAliases(),
   };
 };

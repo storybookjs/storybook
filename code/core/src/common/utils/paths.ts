dbc6ee9c1b1c72ef429b9485cdc67a045a9f0dd6
Gert Hengeveld <info@ghengeveld.nl>
2025-07-15 17:35:35 +0200

    More logging

--- code/core/src/common/utils/paths.ts
+++ code/core/src/common/utils/paths.ts
@@ -8,45 +8,36 @@ let projectRoot: string | undefined;
 
 export const getProjectRoot = () => {
   if (projectRoot) {
+    process.stdout.write(`\nfound cached projectRoot: ${projectRoot}\n`);
     return projectRoot;
   }
 
   let result;
   // Allow manual override in cases where auto-detect doesn't work
   if (process.env.STORYBOOK_PROJECT_ROOT) {
+    process.stdout.write(`\nusing STORYBOOK_PROJECT_ROOT: ${process.env.STORYBOOK_PROJECT_ROOT}\n`);
     return process.env.STORYBOOK_PROJECT_ROOT;
   }
 
   try {
-    const found = findUpSync('.git', { type: 'directory' });
+    process.stdout.write(`\nsearching for repository root\n`);
+    const found =
+      findUpSync('.git', { type: 'directory' }) ||
+      findUpSync('.svn', { type: 'directory' }) ||
+      findUpSync('.hg', { type: 'directory' });
     if (found) {
+      process.stdout.write(`\nfound repository root: ${found}\n`);
       result = join(found, '..');
     }
   } catch (e) {
-    //
-  }
-
-  try {
-    const found = findUpSync('.svn', { type: 'directory' });
-    if (found) {
-      result = result || join(found, '..');
-    }
-  } catch (e) {
-    //
-  }
-
-  try {
-    const found = findUpSync('.hg', { type: 'directory' });
-    if (found) {
-      result = result || join(found, '..');
-    }
-  } catch (e) {
-    //
+    process.stdout.write(`\nerror searching for repository root: ${e}\n`);
   }
 
   try {
     const splitDirname = __dirname.split('node_modules');
+    process.stdout.write(`\nsplitDirname: ${splitDirname}\n`);
     const isSplitDirnameReachable = !relative(splitDirname[0], process.cwd()).startsWith('..');
+    process.stdout.write(`\nisSplitDirnameReachable: ${isSplitDirnameReachable}\n`);
     result =
       result ||
       (isSplitDirnameReachable
@@ -55,20 +46,23 @@ export const getProjectRoot = () => {
           : undefined
         : undefined);
   } catch (e) {
-    //
+    process.stdout.write(`\nerror searching for splitDirname: ${e}\n`);
   }
 
   try {
+    process.stdout.write(`\nsearching for lock file\n`);
     const found = findUpSync(LOCK_FILES, {
       type: 'file',
     });
     if (found) {
+      process.stdout.write(`\nfound lock file: ${found}\n`);
       result = result || join(found, '..');
     }
   } catch (e) {
-    //
+    process.stdout.write(`\nerror searching for lock file: ${e}\n`);
   }
 
+  process.stdout.write(`\nresult: ${result}\n`);
   projectRoot = result || process.cwd();
 
   return projectRoot;

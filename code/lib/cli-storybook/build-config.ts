e6c5f02a53327cfd8bb805fdb5f24ac7a5920c0b
Jeppe Reinhold <jeppe@reinhold.is>
2025-07-17 11:28:51 +0200

    split out build configurations to their respective packages

---
+++ code/lib/cli-storybook/build-config.ts
@@ -0,0 +1,20 @@
+import { join } from 'node:path';
+
+import type { BuildEntries } from '../../scripts/build/utils/entry-utils';
+
+const config: BuildEntries = {
+  entries: {
+    node: [
+      {
+        entryPoint: './src/bin/index.ts',
+        dts: false,
+      },
+    ],
+  },
+  postbuild: async (cwd) => {
+    const { chmod } = await import('node:fs/promises');
+    await chmod(join(cwd, 'dist', 'bin', 'index.js'), 0o755);
+  },
+};
+
+export default config;

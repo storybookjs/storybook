a08989ba5583395a54de9341b3835e4d7a8a39ad
Michael Shilman <shilman@users.noreply.github.com>
2025-08-01 09:51:16 +0800

    Fix test install in agentic environments

--- code/lib/create-storybook/src/initiate.ts
+++ code/lib/create-storybook/src/initiate.ts
@@ -476,7 +476,8 @@ export async function doInitiate(options: CommandOptions): Promise<
   let selectedFeatures = new Set<GeneratorFeature>(options.features || []);
   if (installType === 'recommended') {
     selectedFeatures.add('docs');
-    if (isInteractive) {
+    // Don't install in CI but install in non-TTY environments like agentic installs
+    if (!isCI()) {
       selectedFeatures.add('test');
     }
     if (newUser) {

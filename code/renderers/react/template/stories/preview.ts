4e90611b11bc35e2be9d529ecf37f30e0cb91bb3
Kasper Peulen <kasperpeulen@gmail.com>
2025-08-20 14:54:39 +0200

    Fix types

--- code/renderers/react/template/stories/preview.ts
+++ code/renderers/react/template/stories/preview.ts
@@ -1,5 +1,4 @@
 import { __definePreview } from '@storybook/react';
 
 // This file is used for CSF4 stories
-// @ts-expect-error TODO: Fix later with Kasper
-export default __definePreview({});
+export default __definePreview({ addons: [] });

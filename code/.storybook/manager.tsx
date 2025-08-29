6f1bb0fe7766cbed6cca61ae54aeb49fb8d533c4
Steve Dodier-Lazaro <sidnioulz@gmail.com>
2025-08-29 17:51:30 +0200

    Core: Update migration.md, move react-aria to pre-bundled deps, clean up local sb manager

--- code/.storybook/manager.tsx
+++ code/.storybook/manager.tsx
@@ -1,18 +1,8 @@
-import React from 'react';
-
 import { startCase } from 'es-toolkit/compat';
-import { addons, types } from 'storybook/manager-api';
+import { addons } from 'storybook/manager-api';
 
 addons.setConfig({
   sidebar: {
     renderLabel: ({ name, type }) => (type === 'story' ? name : startCase(name)),
   },
-});
-
-addons.register('storybook/internal/manager', (api) => {
-  addons.add('DEBUG-A11y', {
-    type: types.TAB,
-    title: 'DEBUG A11y',
-    render: () => <div>Keep this tab until you're done debugging.</div>,
-  });
 });

9bf733aec28b5e738712da3bb437797fdd0839db
Steve Dodier-Lazaro <sidnioulz@gmail.com>
2025-09-03 22:10:22 +0200

    UI: Hide shortcut tooltips when enableShortcuts is false

--- code/core/src/components/components/tooltip/InteractiveTooltipWrapper.tsx
+++ code/core/src/components/components/tooltip/InteractiveTooltipWrapper.tsx
@@ -13,18 +13,26 @@ const NoMarginNote = styled(TooltipNote)(() => ({
 
 // TODO: Improve delay management; make the delay near-instantaneous if any instance of this component has been recently shown.
 // TODO: Find way to trigger the tooltip when the child component is focused too. Will need this for general a11y but particularly for Sidebar nav secondary actions.
-
 export const InteractiveTooltipWrapper: React.FC<{
   children: React.ReactNode;
   shortcut?: API_KeyCollection;
   tooltip?: string;
 }> = ({ children, shortcut, tooltip }) => {
   const tooltipLabel = useMemo(() => {
-    if (!tooltip && !shortcut) {
+    // We read from document despite the lack of reactivity, because this
+    // option isn't changeable in the UI. If it was, we'd need to fetch the
+    // addons singleton. This component is used in Buttons, etc., which are
+    // public API and can be imported in MDX. So We rely on a declarative
+    // DOM attribute instead of relying on the manager API.
+    const hasShortcuts = document.body.getAttribute('data-shortcuts-enabled') === 'true';
+
+    if (!tooltip && (!shortcut || !hasShortcuts)) {
       return undefined;
     }
 
-    return [tooltip, shortcut && `[${shortcutToHumanString(shortcut)}]`].filter(Boolean).join(' ');
+    return [tooltip, shortcut && hasShortcuts && `[${shortcutToHumanString(shortcut)}]`]
+      .filter(Boolean)
+      .join(' ');
   }, [shortcut, tooltip]);
 
   return tooltipLabel ? (

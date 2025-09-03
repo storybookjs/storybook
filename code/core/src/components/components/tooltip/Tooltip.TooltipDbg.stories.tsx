38bb83d18a19955816d2a357686c30ad7d2eda6c
Steve Dodier-Lazaro <sidnioulz@gmail.com>
2025-09-03 23:05:47 +0200

    refactor: Remove mock story file created in the UI and added to Git by accident

--- code/core/src/components/components/tooltip/Tooltip.TooltipDbg.stories.tsx
+++
@@ -1,9 +0,0 @@
-import preview from '#.storybook/preview';
-
-import { TooltipDbg } from './Tooltip';
-
-const meta = preview.meta({
-  component: TooltipDbg,
-});
-
-export const Default = meta.story({});

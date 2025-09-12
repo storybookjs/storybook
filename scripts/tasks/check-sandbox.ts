f34122d3561ef4245c139b2b0df9acd40e76ca58
Bill Collins <bill@bill.works>
2025-09-12 08:55:38 +0100

    Create check-sandbox task

---
+++ scripts/tasks/check-sandbox.ts
@@ -0,0 +1,24 @@
+import { access } from 'node:fs/promises';
+
+import type { Task } from '../task';
+import { exec } from '../utils/exec';
+
+async function pathExists(path: string) {
+  try {
+    await access(path);
+    return true;
+  } catch {
+    return false;
+  }
+}
+
+export const checkSandbox: Task = {
+  description: 'Typecheck the a sandbox',
+  dependsOn: ['sandbox'],
+  async ready({ builtSandboxDir }) {
+    return pathExists(builtSandboxDir);
+  },
+  async run({ sandboxDir }, { dryRun, debug }) {
+    await exec(`yarn tsc -p tsconfig.app.json`, { cwd: sandboxDir }, { dryRun, debug });
+  },
+};

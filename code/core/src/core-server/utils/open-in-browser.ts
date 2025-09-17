b7f1e1b3972f7e4d77632320b6c8e06f603d442b
Norbert de Langen <ndelangen@me.com>
2025-09-17 11:39:48 +0200

    Add AppleScript for reusing existing Chrome tabs and enhance browser opening logic
    - Introduced `openBrowser.applescript` to manage tab reuse in Google Chrome on macOS.
    - Updated `openInBrowser` function to utilize the new `openBrowser` utility for improved error handling.
    - Created `opener.ts` to encapsulate browser opening logic, including handling different browser environments and executing scripts.

--- code/core/src/core-server/utils/open-in-browser.ts
+++ code/core/src/core-server/utils/open-in-browser.ts
@@ -3,10 +3,25 @@ import { logger } from 'storybook/internal/node-logger';
 import open from 'open';
 import { dedent } from 'ts-dedent';
 
+import { openBrowser } from './open-browser/opener';
+
 export async function openInBrowser(address: string) {
+  let errorOccured = false;
+
+  try {
+    await openBrowser(address);
+  } catch (error) {
+    errorOccured = true;
+  }
+
   try {
     await open(address);
+    errorOccured = false;
   } catch (error) {
+    errorOccured = true;
+  }
+
+  if (errorOccured) {
     logger.error(dedent`
         Could not open ${address} inside a browser. If you're running this command inside a
         docker container or on a CI, you need to pass the '--ci' flag to prevent opening a

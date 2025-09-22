85a24624dc908bd050160640f6aecf31b65483f5
Steve Dodier-Lazaro <sidnioulz@gmail.com>
2025-09-22 19:22:38 +0200

    UI: Fix type error in deprecated tab hooks again

--- code/core/src/components/components/tabs/tabs.hooks.tsx
+++ code/core/src/components/components/tabs/tabs.hooks.tsx
@@ -69,7 +69,6 @@ export function useList(list: ChildrenListComplete) {
       return (
         <>
           <WithTooltip
-            interactive
             visible={isTooltipVisible}
             onVisibleChange={setTooltipVisible}
             placement="bottom"

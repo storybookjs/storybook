321d812a2fa024f039635e72c34911693ec203a3
Norbert de Langen <ndelangen@me.com>
2025-09-24 13:29:55 +0200

    remove re-throwing error

--- code/core/src/shared/utils/module.ts
+++ code/core/src/shared/utils/module.ts
@@ -121,9 +121,5 @@ export const safeResolveModule = ({
     if (statSync(resolvedPath).isFile()) {
       return resolvedPath;
     }
-  } catch (e: any) {
-    if (e.code !== 'ERR_MODULE_NOT_FOUND') {
-      throw e;
-    }
-  }
+  } catch (e: any) {}
 };

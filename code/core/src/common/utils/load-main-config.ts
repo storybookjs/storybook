4da70a68b5181838d208536525c9fc6e0d9c5d76
Norbert de Langen <ndelangen@me.com>
2025-09-25 11:52:29 +0200

    add fallback to loadMainConfig by adding migration assistant header

--- code/core/src/common/utils/load-main-config.ts
+++ code/core/src/common/utils/load-main-config.ts
@@ -1,7 +1,10 @@
+import { readFile, writeFile } from 'node:fs/promises';
 import { relative, resolve } from 'node:path';
 
 import { MainFileEvaluationError } from 'storybook/internal/server-errors';
 import type { StorybookConfig } from 'storybook/internal/types';
+
+import { dedent } from 'ts-dedent';
 
 import { importModule } from '../../shared/utils/module';
 import { getInterpretedFile } from './interpret-files';
@@ -24,6 +27,21 @@ export async function loadMainConfig({
   } catch (e) {
     if (!(e instanceof Error)) {
       throw e;
+    }
+    if (e.message.includes('require is not defined')) {
+      const header = dedent`
+      import { createRequire } from "node:module";
+      import { dirname } from "node:path";
+      import { fileURLToPath } from "node:url";
+
+      const __filename = fileURLToPath(import.meta.url);
+      const __dirname = dirname(__filename);
+      const require = createRequire(import.meta.url);
+      // end of storybook 10 migration assistant header, you can delete the above code
+    `;
+      const content = await readFile(mainPath, 'utf-8');
+      await writeFile(mainPath, [header, content].join('\n\n'));
+      return loadMainConfig({ configDir, cwd });
     }
 
     throw new MainFileEvaluationError({

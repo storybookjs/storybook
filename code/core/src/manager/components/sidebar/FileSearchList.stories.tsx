5b1b47095d6d99ef6e6dc0a9177538a51e54d926
Steve Dodier-Lazaro <sidnioulz@gmail.com>
2025-09-26 12:43:31 +0200

    Tests: Try to reinstate FileSearchModal story as it's used as a dep elsewhere

--- code/core/src/manager/components/sidebar/FileSearchList.stories.tsx
+++ code/core/src/manager/components/sidebar/FileSearchList.stories.tsx
@@ -37,125 +37,124 @@ export const Empty: Story = {
   },
 };
 
-// https://github.com/storybookjs/storybook/issues/32552
-// export const WithResults: Story = {
-//   play: async ({ canvasElement, args }) => {
-//     // use react testing library
-//     // select first item in the list and click on it
-//     const firstItem = await findByText(canvasElement, 'module-multiple-exports.js');
-//     fireEvent.click(firstItem);
+export const WithResults: Story = {
+  play: async ({ canvasElement, args }) => {
+    // use react testing library
+    // select first item in the list and click on it
+    const firstItem = await findByText(canvasElement, 'module-multiple-exports.js');
+    fireEvent.click(firstItem);
 
-//     const exportedElement1 = await findByText(canvasElement, 'module-multiple-exports');
-//     fireEvent.click(exportedElement1);
+    const exportedElement1 = await findByText(canvasElement, 'module-multiple-exports');
+    fireEvent.click(exportedElement1);
 
-//     expect(args.onNewStory).toHaveBeenCalledWith(
-//       expect.objectContaining({
-//         selectedItemId: 'src/module-multiple-exports.js_0',
-//         componentExportName: 'default',
-//         componentFilePath: 'src/module-multiple-exports.js',
-//         componentIsDefaultExport: true,
-//       })
-//     );
+    expect(args.onNewStory).toHaveBeenCalledWith(
+      expect.objectContaining({
+        selectedItemId: 'src/module-multiple-exports.js_0',
+        componentExportName: 'default',
+        componentFilePath: 'src/module-multiple-exports.js',
+        componentIsDefaultExport: true,
+      })
+    );
 
-//     const exportedElement2 = await findByText(canvasElement, 'namedExport');
-//     fireEvent.click(exportedElement2);
+    const exportedElement2 = await findByText(canvasElement, 'namedExport');
+    fireEvent.click(exportedElement2);
 
-//     expect(args.onNewStory).toHaveBeenCalledWith(
-//       expect.objectContaining({
-//         selectedItemId: 'src/module-multiple-exports.js_1',
-//         componentExportName: 'namedExport',
-//         componentFilePath: 'src/module-multiple-exports.js',
-//         componentIsDefaultExport: false,
-//       })
-//     );
+    expect(args.onNewStory).toHaveBeenCalledWith(
+      expect.objectContaining({
+        selectedItemId: 'src/module-multiple-exports.js_1',
+        componentExportName: 'namedExport',
+        componentFilePath: 'src/module-multiple-exports.js',
+        componentIsDefaultExport: false,
+      })
+    );
 
-//     const singleExport = await findByText(canvasElement, 'module-single-export.js');
-//     fireEvent.click(singleExport);
+    const singleExport = await findByText(canvasElement, 'module-single-export.js');
+    fireEvent.click(singleExport);
 
-//     expect(args.onNewStory).toHaveBeenCalledWith(
-//       expect.objectContaining({
-//         selectedItemId: 'src/module-single-export.js',
-//         componentExportName: 'default',
-//         componentFilePath: 'src/module-single-export.js',
-//         componentIsDefaultExport: true,
-//       })
-//     );
+    expect(args.onNewStory).toHaveBeenCalledWith(
+      expect.objectContaining({
+        selectedItemId: 'src/module-single-export.js',
+        componentExportName: 'default',
+        componentFilePath: 'src/module-single-export.js',
+        componentIsDefaultExport: true,
+      })
+    );
 
-//     expect(args.onNewStory).toHaveBeenCalledTimes(3);
+    expect(args.onNewStory).toHaveBeenCalledTimes(3);
 
-//     const noExportsModule1 = await findByText(canvasElement, 'no-exports-module.js');
-//     fireEvent.click(noExportsModule1);
+    const noExportsModule1 = await findByText(canvasElement, 'no-exports-module.js');
+    fireEvent.click(noExportsModule1);
 
-//     expect(args.onNewStory).toHaveBeenCalledTimes(3);
+    expect(args.onNewStory).toHaveBeenCalledTimes(3);
 
-//     const noExportsModule2 = await findByText(canvasElement, 'no-exports-module-1.js');
-//     fireEvent.click(noExportsModule2);
+    const noExportsModule2 = await findByText(canvasElement, 'no-exports-module-1.js');
+    fireEvent.click(noExportsModule2);
 
-//     expect(args.onNewStory).toHaveBeenCalledTimes(3);
-//   },
-//   args: {
-//     isLoading: false,
-//     // @ts-expect-error (non strict)
-//     errorItemId: null,
-//     searchResults: [
-//       {
-//         exportedComponents: [],
-//         storyFileExists: false,
-//         filepath: 'src/no-exports-module.js',
-//       },
-//       {
-//         storyFileExists: false,
-//         exportedComponents: [
-//           {
-//             default: true,
-//             name: 'default',
-//           },
-//           {
-//             default: false,
-//             name: 'namedExport',
-//           },
-//         ],
-//         filepath: 'src/module-multiple-exports.js',
-//       },
-//       {
-//         storyFileExists: false,
-//         exportedComponents: null,
-//         filepath: 'src/no-exports-module-1.js',
-//       },
-//       {
-//         storyFileExists: false,
-//         exportedComponents: [
-//           {
-//             default: true,
-//             name: 'default',
-//           },
-//         ],
-//         filepath: 'src/module-single-export.js',
-//       },
-//       {
-//         storyFileExists: true,
-//         exportedComponents: [
-//           {
-//             default: true,
-//             name: 'default',
-//           },
-//           {
-//             default: false,
-//             name: 'namedExportWithStory',
-//           },
-//         ],
-//         filepath: 'src/has-story-file-with-multiple-exports.js',
-//       },
-//       {
-//         storyFileExists: true,
-//         exportedComponents: [
-//           {
-//             default: true,
-//             name: 'default',
-//           },
-//         ],
-//         filepath: 'src/has-story-file.js',
-//       },
-//     ],
-//   },
-// };
+    expect(args.onNewStory).toHaveBeenCalledTimes(3);
+  },
+  args: {
+    isLoading: false,
+    // @ts-expect-error (non strict)
+    errorItemId: null,
+    searchResults: [
+      {
+        exportedComponents: [],
+        storyFileExists: false,
+        filepath: 'src/no-exports-module.js',
+      },
+      {
+        storyFileExists: false,
+        exportedComponents: [
+          {
+            default: true,
+            name: 'default',
+          },
+          {
+            default: false,
+            name: 'namedExport',
+          },
+        ],
+        filepath: 'src/module-multiple-exports.js',
+      },
+      {
+        storyFileExists: false,
+        exportedComponents: null,
+        filepath: 'src/no-exports-module-1.js',
+      },
+      {
+        storyFileExists: false,
+        exportedComponents: [
+          {
+            default: true,
+            name: 'default',
+          },
+        ],
+        filepath: 'src/module-single-export.js',
+      },
+      {
+        storyFileExists: true,
+        exportedComponents: [
+          {
+            default: true,
+            name: 'default',
+          },
+          {
+            default: false,
+            name: 'namedExportWithStory',
+          },
+        ],
+        filepath: 'src/has-story-file-with-multiple-exports.js',
+      },
+      {
+        storyFileExists: true,
+        exportedComponents: [
+          {
+            default: true,
+            name: 'default',
+          },
+        ],
+        filepath: 'src/has-story-file.js',
+      },
+    ],
+  },
+};

b9813a9336810ae537835c6819c1f07118c7b4da
Valentin Palkovic <valentin@chromatic.com>
2025-10-09 09:58:04 +0200

    Update react kitchen-sink to use Vitest 4

--- test-storybooks/portable-stories-kitchen-sink/react/vite.config.ts
+++ test-storybooks/portable-stories-kitchen-sink/react/vite.config.ts
@@ -1,7 +1,43 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
+import { storybookTest } from "@storybook/addon-vitest/vitest-plugin";
+import { playwright } from "@vitest/browser-playwright";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    storybookTest(
+      process.env.SKIP_FAIL_ON_PURPOSE
+        ? {
+            tags: {
+              exclude: ["fail-on-purpose"],
+            },
+          }
+        : undefined
+    ),
+  ],
+  test: {
+    name: "storybook",
+    pool: "threads",
+    deps: {
+      optimizer: {
+        web: {
+          enabled: false,
+        },
+      },
+    },
+    browser: {
+      enabled: true,
+      provider: playwright({}),
+      headless: true,
+      instances: [
+        {
+          browser: "chromium",
+        },
+      ],
+    },
+    setupFiles: ["./.storybook/vitest.setup.ts"],
+    environment: "jsdom",
+  },
+});

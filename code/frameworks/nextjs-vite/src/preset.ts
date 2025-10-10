48fb2b6ca3e2e2170b52df6cf96a9da37e087979
Norbert de Langen <ndelangen@me.com>
2025-10-10 14:57:02 +0200

    Merge pull request #32691 from storybookjs/norbert/postcss-config-modification
    NextJS: Enhance PostCSS configuration handling
    (cherry picked from commit e9a2317cea0c3b0fb25de5b63ddd6531e547abfb)

--- code/frameworks/nextjs-vite/src/preset.ts
+++ code/frameworks/nextjs-vite/src/preset.ts
@@ -1,16 +1,14 @@
 // https://storybook.js.org/docs/react/addons/writing-presets
 import { dirname, join } from 'node:path';
 
-import { getProjectRoot } from 'storybook/internal/common';
-import { IncompatiblePostCssConfigError } from 'storybook/internal/server-errors';
 import type { PresetProperty } from 'storybook/internal/types';
 
 import type { StorybookConfigVite } from '@storybook/builder-vite';
 import { viteFinal as reactViteFinal } from '@storybook/react-vite/preset';
 
-import postCssLoadConfig from 'postcss-load-config';
 import vitePluginStorybookNextjs from 'vite-plugin-storybook-nextjs';
 
+import { normalizePostCssConfig } from './find-postcss-config';
 import type { FrameworkOptions } from './types';
 
 export const core: PresetProperty<'core'> = async (config, options) => {
@@ -46,17 +44,11 @@ export const optimizeViteDeps = [
 export const viteFinal: StorybookConfigVite['viteFinal'] = async (config, options) => {
   const reactConfig = await reactViteFinal(config, options);
 
-  try {
-    const inlineOptions = config.css?.postcss;
-    const searchPath = typeof inlineOptions === 'string' ? inlineOptions : config.root;
-    await postCssLoadConfig({}, searchPath, { stopDir: getProjectRoot() });
-  } catch (e: any) {
-    if (!e.message.includes('No PostCSS Config found')) {
-      // This is a custom error that we throw when the PostCSS config is invalid
-      if (e.message.includes('Invalid PostCSS Plugin found')) {
-        throw new IncompatiblePostCssConfigError({ error: e });
-      }
-    }
+  const inlineOptions = config.css?.postcss;
+  const searchPath = typeof inlineOptions === 'string' ? inlineOptions : config.root;
+
+  if (searchPath) {
+    await normalizePostCssConfig(searchPath);
   }
 
   const { nextConfigPath } = await options.presets.apply<FrameworkOptions>('frameworkOptions');

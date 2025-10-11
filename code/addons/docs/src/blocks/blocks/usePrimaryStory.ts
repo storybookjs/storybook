ef59364e83a44b42e75ccb6f5b10358c643b35b2
ia319 <linglingmeng876@gmail.com>
2025-10-12 01:56:02 +0800

    docs(blocks): clarify comment for usePrimaryStory hook

--- code/addons/docs/src/blocks/blocks/usePrimaryStory.ts
+++ code/addons/docs/src/blocks/blocks/usePrimaryStory.ts
@@ -5,8 +5,8 @@ import type { PreparedStory } from 'storybook/internal/types';
 import { DocsContext } from './DocsContext';
 
 /**
- * A hook to get the primary story for the current component It filters out stories with the
- * '!autodocs' tag.
+ * A hook to get the primary story for the current component's doc page. It defines the primary
+ * story as the first story that includes the 'autodocs' tag
  */
 export const usePrimaryStory = (): PreparedStory | undefined => {
   const context = useContext(DocsContext);

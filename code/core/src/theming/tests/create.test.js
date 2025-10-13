7760b5b673b6f3890a4087e5eaed680ae1642445
Steve Dodier-Lazaro <sidnioulz@gmail.com>
2025-10-13 20:18:36 +0200

    Tests: Fix missing import in PR code

--- code/core/src/theming/tests/create.test.js
+++ code/core/src/theming/tests/create.test.js
@@ -1,4 +1,4 @@
-import { describe, expect, it, vi } from 'vitest';
+import { beforeEach, describe, expect, it, vi } from 'vitest';
 
 import { create } from '../create';
 import { getPreferredColorScheme } from './../utils';

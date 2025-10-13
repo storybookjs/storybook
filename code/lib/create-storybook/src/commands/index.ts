7bff23bc0a0232b3387573021153746d74a76e3c
Valentin Palkovic <valentin@chromatic.com>
2025-10-13 09:57:40 +0200

    Add command and module exports for Storybook initialization workflow

---
+++ code/lib/create-storybook/src/commands/index.ts
@@ -0,0 +1,23 @@
+/**
+ * Command classes for Storybook initialization workflow
+ *
+ * Each command represents a discrete step in the init process with clear responsibilities
+ */
+
+export { PreflightCheckCommand } from './PreflightCheckCommand';
+export type { PreflightCheckResult } from './PreflightCheckCommand';
+
+export { UserPreferencesCommand } from './UserPreferencesCommand';
+export type { InstallType, UserPreferencesOptions, UserPreferencesResult } from './UserPreferencesCommand';
+
+export { ProjectDetectionCommand } from './ProjectDetectionCommand';
+
+export { GeneratorExecutionCommand } from './GeneratorExecutionCommand';
+export type { GeneratorExecutionResult } from './GeneratorExecutionCommand';
+
+export { AddonConfigurationCommand } from './AddonConfigurationCommand';
+
+export { DependencyInstallationCommand } from './DependencyInstallationCommand';
+
+export { FinalizationCommand } from './FinalizationCommand';
+

e4f7a57be9498d8a1611d05b6a563488185a1719
Kasper Peulen <kasperpeulen@gmail.com>
2025-10-20 13:20:25 +0200

    Enable in UI

--- code/.storybook/main.ts
+++ code/.storybook/main.ts
@@ -137,6 +137,7 @@ const config = defineMain({
   features: {
     developmentModeForBuild: true,
     experimentalTestSyntax: true,
+    experimental_componentsManifest: true,
   },
   staticDirs: [{ from: './bench/bundle-analyzer', to: '/bundle-analyzer' }],
   viteFinal: async (viteConfig, { configType }) => {

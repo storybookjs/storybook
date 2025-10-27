84221677cd0c2dfe560a242939508343cb5de41e
Kasper Peulen <kasperpeulen@gmail.com>
2025-10-27 11:25:48 +0100

    Be safe

--- code/renderers/react/src/entry-preview-docs.ts
+++ code/renderers/react/src/entry-preview-docs.ts
@@ -1,15 +1,10 @@
 import type { DecoratorFunction } from 'storybook/internal/types';
-import type { StorybookConfigRaw } from 'storybook/internal/types';
 
 import { jsxDecorator } from './docs/jsxDecorator';
 import type { ReactRenderer } from './types';
 
-// eslint-disable-next-line no-var
-declare var FEATURES: NonNullable<StorybookConfigRaw['features']>;
-
-export const decorators: DecoratorFunction<ReactRenderer>[] = FEATURES.experimentalCodeExamples
-  ? []
-  : [jsxDecorator];
+export const decorators: DecoratorFunction<ReactRenderer>[] =
+  'FEATURES' in globalThis && globalThis?.FEATURES?.experimentalCodeExamples ? [] : [jsxDecorator];
 
 export { applyDecorators } from './docs/applyDecorators';
 

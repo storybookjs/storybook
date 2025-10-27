a496f5638d65c636cf52f091a4af5a762746f547
Kasper Peulen <kasperpeulen@gmail.com>
2025-10-27 11:04:08 +0100

    Fix portable stories test

--- test-storybooks/portable-stories-kitchen-sink/react/jest.setup.ts
+++ test-storybooks/portable-stories-kitchen-sink/react/jest.setup.ts
@@ -1,7 +1,5 @@
-import "@testing-library/jest-dom";
-import { setProjectAnnotations } from "@storybook/react-vite";
-import sbAnnotations from "./.storybook/preview";
-
-globalThis.FEATURES ??= {};
+import '@testing-library/jest-dom';
+import { setProjectAnnotations } from '@storybook/react-vite';
+import sbAnnotations from './.storybook/preview';
 
 setProjectAnnotations([sbAnnotations]);

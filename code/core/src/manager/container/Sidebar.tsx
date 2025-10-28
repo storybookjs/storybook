38bfbc15bbc2732f479818e11d3f22f04b8aec00
Gert Hengeveld <info@ghengeveld.nl>
2025-10-28 22:01:56 +0100

    Fix progress updates for guide in sidebar menu

--- code/core/src/manager/container/Sidebar.tsx
+++ code/core/src/manager/container/Sidebar.tsx
@@ -30,18 +30,11 @@ const Sidebar = React.memo(function Sideber({ onMenuClick }: SidebarProps) {
       refs,
     } = state;
 
-    const menu = useMenu({
-      api,
-      showToolbar,
-      isPanelShown: api.getIsPanelShown(),
-      isNavShown: api.getIsNavShown(),
-      enableShortcuts,
-    });
-
     const whatsNewNotificationsEnabled =
       state.whatsNewData?.status === 'SUCCESS' && !state.disableWhatsNewNotifications;
 
     return {
+      api,
       title: name,
       url,
       indexJson: internal_index,
@@ -52,7 +45,9 @@ const Sidebar = React.memo(function Sideber({ onMenuClick }: SidebarProps) {
       storyId,
       refId,
       viewMode,
-      menu,
+      showToolbar,
+      isPanelShown: api.getIsPanelShown(),
+      isNavShown: api.getIsNavShown(),
       menuHighlighted: whatsNewNotificationsEnabled && api.isWhatsNewUnread(),
       enableShortcuts,
     };
@@ -60,11 +55,18 @@ const Sidebar = React.memo(function Sideber({ onMenuClick }: SidebarProps) {
 
   return (
     <Consumer filter={mapper}>
-      {(fromState) => {
+      {({ api, showToolbar, isPanelShown, isNavShown, enableShortcuts, ...state }) => {
+        const menu = useMenu({ api, showToolbar, isPanelShown, isNavShown, enableShortcuts });
         const allStatuses = experimental_useStatusStore();
 
         return (
-          <SidebarComponent {...fromState} allStatuses={allStatuses} onMenuClick={onMenuClick} />
+          <SidebarComponent
+            {...state}
+            menu={menu}
+            onMenuClick={onMenuClick}
+            allStatuses={allStatuses}
+            enableShortcuts={enableShortcuts}
+          />
         );
       }}
     </Consumer>

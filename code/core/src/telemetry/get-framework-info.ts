05c1652a8eafde6ca22e52bb9c4a89f3615e6691
Valentin Palkovic <valentin@chromatic.com>
2025-11-19 16:20:13 +0000

    Refactor getFrameworkInfo to include configDir parameter and update framework metadata retrieval

--- code/core/src/telemetry/get-framework-info.ts
+++ code/core/src/telemetry/get-framework-info.ts
@@ -1,80 +1,18 @@
-import { normalize } from 'node:path';
+import { getStorybookInfo } from 'storybook/internal/common';
+import type { StorybookConfig } from 'storybook/internal/types';
 
-import { frameworkPackages } from 'storybook/internal/common';
-import type { PackageJson, StorybookConfig } from 'storybook/internal/types';
+export async function getFrameworkInfo(mainConfig: StorybookConfig, configDir: string) {
+  const { frameworkPackage, rendererPackage, builderPackage } = await getStorybookInfo(configDir);
 
-import { getActualPackageJson } from './package-json';
-import { cleanPaths } from './sanitize';
-
-const knownRenderers = [
-  'html',
-  'react',
-  'svelte',
-  'vue3',
-  'preact',
-  'server',
-  'vue',
-  'web-components',
-  'angular',
-  'ember',
-];
-
-const knownBuilders = ['builder-webpack5', 'builder-vite'];
-
-function findMatchingPackage(packageJson: PackageJson, suffixes: string[]) {
-  const { name = '', version, dependencies, devDependencies, peerDependencies } = packageJson;
-
-  const allDependencies = {
-    // We include the framework itself because it may be a renderer too (e.g. angular)
-    [name]: version,
-    ...dependencies,
-    ...devDependencies,
-    ...peerDependencies,
-  };
-
-  return suffixes.map((suffix) => `@storybook/${suffix}`).find((pkg) => allDependencies[pkg]);
-}
-
-export const getFrameworkPackageName = (packageNameOrPath: string) => {
-  const normalizedPath = normalize(packageNameOrPath).replace(new RegExp(/\\/, 'g'), '/');
-
-  const knownFramework = Object.keys(frameworkPackages).find((pkg) => normalizedPath.endsWith(pkg));
-
-  return knownFramework || cleanPaths(packageNameOrPath).replace(/.*node_modules[\\/]/, '');
-};
-
-export async function getFrameworkInfo(mainConfig: StorybookConfig) {
-  if (!mainConfig?.framework) {
-    return {};
-  }
-
-  const rawName =
-    typeof mainConfig.framework === 'string' ? mainConfig.framework : mainConfig.framework?.name;
-  if (!rawName) {
-    return {};
-  }
-
-  const frameworkPackageJson = await getActualPackageJson(rawName);
-
-  if (!frameworkPackageJson) {
-    return {};
-  }
-
-  const builder = findMatchingPackage(frameworkPackageJson, knownBuilders);
-  const renderer = findMatchingPackage(frameworkPackageJson, knownRenderers);
-
-  // TODO: Evaluate if this is correct after removing pnp compatibility code in SB11
-  // parse framework name and strip off pnp paths etc.
-  const sanitizedFrameworkName = getFrameworkPackageName(rawName);
   const frameworkOptions =
     typeof mainConfig.framework === 'object' ? mainConfig.framework.options : {};
 
   return {
     framework: {
-      name: sanitizedFrameworkName,
+      name: frameworkPackage,
       options: frameworkOptions,
     },
-    builder,
-    renderer,
+    builder: builderPackage,
+    renderer: rendererPackage,
   };
 }

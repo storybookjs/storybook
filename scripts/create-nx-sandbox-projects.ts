ee68eed0a93b946bb97552e581b4362c651fafdb
Kasper Peulen <kasperpeulen@gmail.com>
2025-11-21 20:34:24 +0100

    Remove svelte skeleton for now

--- scripts/create-nx-sandbox-projects.ts
+++ scripts/create-nx-sandbox-projects.ts
@@ -71,25 +71,27 @@ const projectJson = (name: string, framework: string, tags: string[], template: 
   },
   tags,
 });
-Object.entries(allTemplates).forEach(([key, value]) => {
-  const p = key.replaceAll('/', '-');
-  const full = join(process.cwd(), '../code/sandbox', p, 'project.json');
+Object.entries(allTemplates)
+  .filter(([key]) => key !== 'svelte-kit/skeleton-ts')
+  .forEach(([key, value]) => {
+    const p = key.replaceAll('/', '-');
+    const full = join(process.cwd(), '../code/sandbox', p, 'project.json');
 
-  console.log(full);
-  const framework = value.expected.framework.replace('@storybook/', '');
-  console.log(framework);
-  console.log();
-  const tags = [
-    ...(normal.includes(key as any) && !value.inDevelopment ? ['ci:normal'] : []),
-    ...(merged.includes(key as any) && !value.inDevelopment ? ['ci:merged'] : []),
-    ...(daily.includes(key as any) && !value.inDevelopment ? ['ci:daily'] : []),
-  ];
-  ensureDirectoryExistence(full);
-  console.log(full);
-  writeFileSync(full, JSON.stringify(projectJson(key, framework, tags, value), null, 2), {
-    encoding: 'utf-8',
+    console.log(full);
+    const framework = value.expected.framework.replace('@storybook/', '');
+    console.log(framework);
+    console.log();
+    const tags = [
+      ...(normal.includes(key as any) && !value.inDevelopment ? ['ci:normal'] : []),
+      ...(merged.includes(key as any) && !value.inDevelopment ? ['ci:merged'] : []),
+      ...(daily.includes(key as any) && !value.inDevelopment ? ['ci:daily'] : []),
+    ];
+    ensureDirectoryExistence(full);
+    console.log(full);
+    writeFileSync(full, JSON.stringify(projectJson(key, framework, tags, value), null, 2), {
+      encoding: 'utf-8',
+    });
   });
-});
 
 function ensureDirectoryExistence(filePath: string): void {
   const dir = dirname(filePath);

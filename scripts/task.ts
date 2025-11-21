c4c70a1ef7f438844d66db13827fef82e551ce4b
Kasper Peulen <kasperpeulen@gmail.com>
2025-11-21 23:45:17 +0100

    Fix logic

--- scripts/task.ts
+++ scripts/task.ts
@@ -498,7 +498,7 @@ async function run() {
 
     let shouldRun = startFrom !== 'never' ? status === 'unready' : finalTask === task;
 
-    if (startFrom !== 'never' && status === 'notserving') {
+    if (status === 'notserving') {
       shouldRun =
         finalTask === task ||
         !!tasksThatDepend.get(task).find((t) => statuses.get(t) === 'unready');
--- scripts/tasks/run-registry.ts
+++ scripts/tasks/run-registry.ts
@@ -1,4 +1,5 @@
 import detectFreePort from 'detect-port';
+import waitOn from 'wait-on';
 
 import type { Task } from '../task';
 import { CODE_DIRECTORY } from '../utils/constants';
@@ -7,19 +8,31 @@ import { exec } from '../utils/exec';
 export async function runRegistry({ dryRun, debug }: { dryRun?: boolean; debug?: boolean }) {
   const controller = new AbortController();
 
-  exec(
-    'yarn local-registry --open',
-    { cwd: CODE_DIRECTORY, env: { CI: 'true' } },
-    { dryRun, debug, signal: controller.signal }
-  ).catch((err) => {
-    // If aborted, we want to make sure the rejection is handled.
+  if ((await detectFreePort(REGISTRY_PORT)) === REGISTRY_PORT) {
+    exec(
+      'yarn local-registry --open',
+      { cwd: CODE_DIRECTORY, env: { CI: 'true' } },
+      { dryRun, debug, signal: controller.signal }
+    ).catch((err) => {
+      // If aborted, we want to make sure the rejection is handled.
 
-    // If aborted, we want to make sure the rejection is handled.
-    if (!err.killed) {
-      throw err;
-    }
+      // If aborted, we want to make sure the rejection is handled.
+      if (!err.killed) {
+        throw err;
+      }
+    });
+    await waitOn({
+      resources: ['http://localhost:6001', 'http://localhost:6002'],
+      interval: 16,
+      timeout: 10000,
+    });
+  }
+
+  await waitOn({
+    resources: ['http://localhost:6001', 'http://localhost:6002'],
+    interval: 16,
+    timeout: 10000,
   });
-  await exec('yarn wait-on tcp:127.0.0.1:6001', { cwd: CODE_DIRECTORY }, { dryRun, debug });
 
   return controller;
 }

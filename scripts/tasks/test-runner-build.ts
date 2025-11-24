7dd976bb9c8e3c21ad8fdf9261e9777a27fae2a6
Kasper Peulen <kasperpeulen@gmail.com>
2025-11-24 11:48:49 +0100

    Increase timeout

--- scripts/tasks/test-runner-build.ts
+++ scripts/tasks/test-runner-build.ts
@@ -25,7 +25,7 @@ export const testRunnerBuild: Task & { port: number } = {
       '--index-json',
     ];
 
-    await waitOn({ resources: [`http://localhost:${port}`], interval: 16, timeout: 200000 });
+    await waitOn({ resources: [`http://localhost:${port}`], interval: 16, timeout: 200001 });
 
     await exec(
       `yarn test-storybook ${flags.join(' ')}`,

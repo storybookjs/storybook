7665aee3c13a623d0a405d2f9c2787bfa6c4b449
Norbert de Langen <ndelangen@me.com>
2025-11-26 09:44:53 +0100

    Update CircleCI configuration to modify E2E test command from 'e2e-tests' to 'e2e-tests-dev' for improved test execution consistency.

--- scripts/ci/data.ts
+++ scripts/ci/data.ts
@@ -1886,7 +1886,7 @@ const jobs = {
         run: {
           command: [
             'TEST_FILES=$(circleci tests glob "code/e2e-tests/*.{test,spec}.{ts,js,mjs}")',
-            'echo "$TEST_FILES" | circleci tests run --command="xargs yarn task e2e-tests --template react-vite/default-ts --no-link -s never" --verbose --index=0 --total=1',
+            'echo "$TEST_FILES" | circleci tests run --command="xargs yarn task e2e-tests-dev --template react-vite/default-ts --no-link -s never" --verbose --index=0 --total=1',
           ].join('\n'),
           name: 'Running E2E Tests',
         },

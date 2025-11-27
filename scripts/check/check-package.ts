0e213243f879ade298c06aa1aaf88e95c9e59143
Bill Collins <bill@bill.works>
2025-11-27 12:05:59 +0000

    Fix type checking on windows

--- scripts/check/check-package.ts
+++ scripts/check/check-package.ts
@@ -1,3 +1,5 @@
+import path from 'node:path';
+
 import { getTSDiagnostics, getTSFilesAndConfig, getTSProgramAndHost } from './utils/typescript';
 
 const tsconfigPath = 'tsconfig.json';
@@ -5,7 +7,7 @@ const tsconfigPath = 'tsconfig.json';
 const { options, fileNames } = getTSFilesAndConfig(tsconfigPath);
 const { program, host } = getTSProgramAndHost(fileNames, options);
 
-const tsDiagnostics = getTSDiagnostics(program, process.cwd(), host);
+const tsDiagnostics = getTSDiagnostics(program, process.cwd().replaceAll(path.sep, '/'), host);
 if (tsDiagnostics.length > 0) {
   console.log(tsDiagnostics);
   process.exit(1);

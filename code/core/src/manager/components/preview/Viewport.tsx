fc46be45bcc4edfbb7604f2187e21f034a8a2df4
Gert Hengeveld <info@ghengeveld.nl>
2025-12-08 17:03:53 +0100

    Add corner drag handle

--- code/core/src/manager/components/preview/Viewport.tsx
+++ code/core/src/manager/components/preview/Viewport.tsx
@@ -11,6 +11,8 @@ import { iconsMap } from '../../../viewport/viewportIcons';
 import { IFrame } from './Iframe';
 import { SizeInput } from './SizeInput';
 
+type DragSide = 'both' | 'bottom' | 'right';
+
 const ViewportWrapper = styled.div<{
   active: boolean;
   isDefault: boolean;
@@ -49,13 +51,13 @@ const Dimensions = styled.div(({ theme }) => ({
 
 const FrameWrapper = styled.div<{
   isDefault: boolean;
-  'data-dragging': 'right' | 'bottom' | undefined;
+  'data-dragging': DragSide | undefined;
 }>(({ isDefault, 'data-dragging': dragging, theme }) => ({
   position: 'relative',
   border: `1px solid ${theme.button.border}`,
   borderWidth: isDefault ? 0 : 1,
   borderRadius: isDefault ? 0 : 4,
-  transition: 'border-color 0.2s',
+  transition: 'border-color 0.2s, box-shadow 0.2s',
   '&:has([data-side="right"]:hover), &[data-dragging="right"]': {
     borderRightColor: theme.color.secondary,
     boxShadow: `4px 0 5px -2px ${theme.background.hoverable}`,
@@ -64,6 +66,33 @@ const FrameWrapper = styled.div<{
     borderBottomColor: theme.color.secondary,
     boxShadow: `0 4px 5px -2px ${theme.background.hoverable}`,
   },
+  '&:has([data-side="both"]:hover), &[data-dragging="both"]': {
+    boxShadow: `3px 3px 5px -2px ${theme.background.hoverable}`,
+    '&::after': {
+      opacity: 1,
+    },
+  },
+  '&::after': {
+    content: '""',
+    display: 'block',
+    position: 'absolute',
+    pointerEvents: 'none',
+    bottom: 1,
+    right: 1,
+    width: 12,
+    height: 12,
+    opacity: 0,
+    transition: 'opacity 0.2s',
+    background: `linear-gradient(to top left,
+      rgba(0,0,0,0) 0%,
+      rgba(0,0,0,0) calc(25% - 1px),
+      ${theme.color.secondary} 25%,
+      rgba(0,0,0,0) calc(25% + 1px),
+      rgba(0,0,0,0) calc(45% - 1px),
+      ${theme.color.secondary} 45%,
+      rgba(0,0,0,0) calc(45% + 1px),
+      rgba(0,0,0,0) 100%)`,
+  },
   iframe: {
     borderRadius: 'inherit',
     pointerEvents: dragging ? 'none' : 'auto',
@@ -71,15 +100,34 @@ const FrameWrapper = styled.div<{
 }));
 
 const DragHandle = styled.div<{
-  'data-side': 'bottom' | 'right';
-}>(({ 'data-side': side }) => ({
-  position: 'absolute',
-  right: side === 'right' ? -12 : 0,
-  bottom: side === 'bottom' ? -12 : 0,
-  width: side === 'right' ? 20 : '100%',
-  height: side === 'bottom' ? 20 : '100%',
-  cursor: side === 'right' ? 'col-resize' : 'row-resize',
-}));
+  'data-side': DragSide;
+}>(
+  { position: 'absolute' },
+  ({ 'data-side': side }) =>
+    side === 'both' && {
+      right: -12,
+      bottom: -12,
+      width: 25,
+      height: 25,
+      cursor: 'nwse-resize',
+    },
+  ({ 'data-side': side }) =>
+    side === 'bottom' && {
+      left: 0,
+      right: 13,
+      bottom: -12,
+      height: 20,
+      cursor: 'row-resize',
+    },
+  ({ 'data-side': side }) =>
+    side === 'right' && {
+      top: 0,
+      right: -12,
+      bottom: 13,
+      width: 20,
+      cursor: 'col-resize',
+    }
+);
 
 export const Viewport = ({
   active,
@@ -108,19 +156,21 @@ export const Viewport = ({
     select,
   } = useViewport();
 
-  const [dragging, setDragging] = useState<undefined | 'right' | 'bottom'>(undefined);
+  const [dragging, setDragging] = useState<DragSide | undefined>(undefined);
   const targetRef = useRef<HTMLDivElement>(null);
   const dragRefX = useRef<HTMLDivElement>(null);
   const dragRefY = useRef<HTMLDivElement>(null);
-  const dragSide = useRef<'bottom' | 'right'>('right');
-  const dragStart = useRef<number | undefined>();
+  const dragRefXY = useRef<HTMLDivElement>(null);
+  const dragSide = useRef<DragSide>('both');
+  const dragStart = useRef<[number, number] | undefined>();
 
   useEffect(() => {
     const onDrag = (e: MouseEvent) => {
-      if (dragSide.current === 'right') {
-        targetRef.current!.style.width = `${dragStart.current! + e.clientX}px`;
-      } else {
-        targetRef.current!.style.height = `${dragStart.current! + e.clientY}px`;
+      if (dragSide.current === 'right' || dragSide.current === 'both') {
+        targetRef.current!.style.width = `${dragStart.current![0] + e.clientX}px`;
+      }
+      if (dragSide.current === 'bottom' || dragSide.current === 'both') {
+        targetRef.current!.style.height = `${dragStart.current![1] + e.clientY}px`;
       }
     };
 
@@ -136,15 +186,15 @@ export const Viewport = ({
       e.preventDefault();
       window.addEventListener('mouseup', onEnd);
       window.addEventListener('mousemove', onDrag);
-      dragSide.current = (e.currentTarget as HTMLElement).dataset.side as 'bottom' | 'right';
-      dragStart.current =
-        dragSide.current === 'right'
-          ? (targetRef.current?.offsetWidth ?? 0) - e.clientX
-          : (targetRef.current?.offsetHeight ?? 0) - e.clientY;
+      dragSide.current = (e.currentTarget as HTMLElement).dataset.side as DragSide;
+      dragStart.current = [
+        (targetRef.current?.offsetWidth ?? 0) - e.clientX,
+        (targetRef.current?.offsetHeight ?? 0) - e.clientY,
+      ];
       setDragging(dragSide.current);
     };
 
-    const handles = [dragRefX.current, dragRefY.current];
+    const handles = [dragRefX.current, dragRefY.current, dragRefXY.current];
     handles.forEach((el) => el?.addEventListener('mousedown', onStart));
     return () => handles.forEach((el) => el?.removeEventListener('mousedown', onStart));
   }, [resize]);
@@ -238,6 +288,7 @@ export const Viewport = ({
         />
         <DragHandle data-side="right" ref={dragRefX} />
         <DragHandle data-side="bottom" ref={dragRefY} />
+        <DragHandle data-side="both" ref={dragRefXY} />
       </FrameWrapper>
     </ViewportWrapper>
   );

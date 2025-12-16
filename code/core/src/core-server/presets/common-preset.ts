25e76c90aadeafe85841c4da0ebecd3598cb4039
Jeppe Reinhold <jeppe@reinhold.is>
2025-12-16 14:49:47 +0100

    remove comment

--- code/core/src/core-server/presets/common-preset.ts
+++ code/core/src/core-server/presets/common-preset.ts
@@ -307,8 +307,6 @@ export const storyIndexGenerator: PresetPropertyFn<
     const workingDir = process.cwd();
     const configDir = options.configDir;
     const stories = await options.presets.apply('stories');
-    // StoryIndexGenerator depends on these normalized stories to be referentially equal
-    // So it's important that we only normalize them once here and pass the same reference around
     const normalizedStories = normalizeStories(stories, {
       configDir,
       workingDir,

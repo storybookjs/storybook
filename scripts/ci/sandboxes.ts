b989368466c54ae08dcb9a48e79233e87f18a784
Norbert de Langen <ndelangen@me.com>
2025-12-19 15:34:48 +0100

    Update working directory path in sandbox flow to use ROOT_DIR for CI configuration

--- scripts/ci/sandboxes.ts
+++ scripts/ci/sandboxes.ts
@@ -195,7 +195,7 @@ export function defineSandboxFlow<K extends string>(name: K) {
                 {
                   run: {
                     name: 'Run prepare',
-                    working_directory: `${WORKING_DIR}/${SANDBOX_DIR}/${id}`,
+                    working_directory: `${ROOT_DIR}/${SANDBOX_DIR}/${id}`,
                     command: `yarn prepare`,
                   },
                 },

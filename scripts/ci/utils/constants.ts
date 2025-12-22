ee41dd4fcf8a345e442b54885e760a8ac576ed78
Norbert de Langen <ndelangen@me.com>
2025-12-22 14:52:31 +0100

    add comment

--- scripts/ci/utils/constants.ts
+++ scripts/ci/utils/constants.ts
@@ -1,4 +1,18 @@
-/** This is the root directory for Linux CI jobs. Windows CI jobs use a different root directory! */
+/**
+ * This is the root directory for Linux CI jobs.
+ *
+ * Windows CI jobs use a different root directory!
+ *
+ * @example /tmp/project/code
+ *
+ * @example C:\Users\circleci\project\code
+ *
+ * @example /tmp/storybook-sandboxes
+ *
+ * @example C:\Users\circleci\storybook-sandboxes
+ *
+ * To ensure the correct paths are used across config generation, we use the following constants.
+ */
 export const ROOT_DIR = '/tmp';
 export const WORKING_DIR = `project`;
 export const SANDBOX_DIR = `storybook-sandboxes`;

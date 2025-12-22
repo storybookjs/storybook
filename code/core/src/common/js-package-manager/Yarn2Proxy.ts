c9a5c7cb381bd190430637e1e27fbf4949f5e3fc
Norbert de Langen <ndelangen@me.com>
2025-12-22 17:31:33 +0100

    deal wth execa upgrade

--- code/core/src/common/js-package-manager/Yarn2Proxy.ts
+++ code/core/src/common/js-package-manager/Yarn2Proxy.ts
@@ -9,7 +9,7 @@ import { PosixFS, VirtualFS, ZipOpenFS } from '@yarnpkg/fslib';
 import { getLibzipSync } from '@yarnpkg/libzip';
 import * as find from 'empathic/find';
 // eslint-disable-next-line depend/ban-dependencies
-import type { ExecaChildProcess } from 'execa';
+import type { ResultPromise } from 'execa';
 
 import { logger } from '../../node-logger';
 import type { ExecuteCommandOptions } from '../utils/command';
@@ -101,7 +101,7 @@ export class Yarn2Proxy extends JsPackageManager {
   public runPackageCommand({
     args,
     ...options
-  }: Omit<ExecuteCommandOptions, 'command'> & { args: string[] }): ExecaChildProcess {
+  }: Omit<ExecuteCommandOptions, 'command'> & { args: string[] }): ResultPromise {
     return executeCommand({
       command: 'yarn',
       args: ['exec', ...args],
@@ -139,7 +139,7 @@ export class Yarn2Proxy extends JsPackageManager {
         },
         cwd: this.instanceDir,
       });
-      const commandResult = childProcess.stdout ?? '';
+      const commandResult = typeof childProcess.stdout === 'string' ? childProcess.stdout : '';
 
       logger.debug(`Installation found for ${pattern.join(', ')}: ${commandResult}`);
 
@@ -251,7 +251,7 @@ export class Yarn2Proxy extends JsPackageManager {
       args: ['config', 'get', 'npmRegistryServer'],
     });
     const result = await process;
-    const url = (result.stdout ?? '').trim();
+    const url = (typeof result.stdout === 'string' ? result.stdout : '').trim();
     return url === 'undefined' ? undefined : url;
   }
 
@@ -267,7 +267,7 @@ export class Yarn2Proxy extends JsPackageManager {
         args: ['npm', 'info', packageName, ...args],
       });
       const result = await process;
-      const commandResult = result.stdout ?? '';
+      const commandResult = typeof result.stdout === 'string' ? result.stdout : '';
 
       const parsedOutput = JSON.parse(commandResult);
       return parsedOutput[field];

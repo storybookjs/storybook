8ea95a994b3b9839406d778213ff62c469b46384
Steve Dodier-Lazaro <sidnioulz@gmail.com>
2025-12-24 14:25:04 +0100

    rewrite getStoryHref docs util to use Web APIs

--- code/addons/docs/src/blocks/getStoryHref.ts
+++ code/addons/docs/src/blocks/getStoryHref.ts
@@ -1,23 +1,15 @@
+// Only for internal use in addon-docs code, because the parent util in `core` cannot be imported.
+// Unlike the parent util, this one only returns the preview URL.
 export const getStoryHref = (storyId: string, additionalParams: Record<string, string> = {}) => {
   const baseUrl = globalThis.PREVIEW_URL || 'iframe.html';
   const [url, paramsStr] = baseUrl.split('?');
-  const params = {
-    ...(paramsStr ? parseQuery(paramsStr) : {}),
-    ...additionalParams,
-    id: storyId,
-  };
-  return `${url}?${Object.entries(params)
-    .map(([key, value]) => `${encodeURIComponent(key)}=${encodeURIComponent(value)}`)
-    .join('&')}`;
+  const params = new URLSearchParams(paramsStr || '');
+
+  Object.entries(additionalParams).forEach(([key, value]) => {
+    params.set(key, value);
+  });
+
+  params.set('id', storyId);
+
+  return `${url}?${params.toString()}`;
 };
-
-function parseQuery(queryString: string) {
-  const query: Record<string, string> = {};
-  const pairs = queryString.split('&');
-
-  for (let i = 0; i < pairs.length; i++) {
-    const pair = pairs[i].split('=');
-    query[decodeURIComponent(pair[0])] = decodeURIComponent(pair[1] || '');
-  }
-  return query;
-}

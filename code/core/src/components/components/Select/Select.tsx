ccbcc9a0c9edae5e7ad62d570edcc9d2b7553373
Gert Hengeveld <info@ghengeveld.nl>
2026-01-09 14:01:41 +0100

    Don't change selection through onFocus handler for SelectOption, because it could change the selection when opening the Select menu

--- code/core/src/components/components/Select/Select.tsx
+++ code/core/src/components/components/Select/Select.tsx
@@ -317,9 +317,9 @@ export const Select = forwardRef<HTMLButtonElement, SelectProps>(
     // wrap setActiveOption to handle selection. We never close the listbox
     // in that scenario.
     const setActiveOption = useCallback(
-      (option: Option | ResetOption) => {
+      (option: Option | ResetOption, changeSelection = true) => {
         setActiveOptionState(optionOrResetToInternal(option));
-        if (!multiSelect) {
+        if (!multiSelect && changeSelection) {
           handleSelectOption(optionOrResetToInternal(option));
         }
       },
@@ -577,7 +577,7 @@ export const Select = forwardRef<HTMLButtonElement, SelectProps>(
                           handleClose();
                         }
                       }}
-                      onFocus={() => setActiveOption(externalOption)}
+                      onFocus={() => setActiveOption(externalOption, false)}
                       shouldLookDisabled={isReset && selectedOptions.length === 0 && multiSelect}
                       onKeyDown={(e: KeyboardEvent) => {
                         if (e.key === 'Enter' || e.key === ' ') {

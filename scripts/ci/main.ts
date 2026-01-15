a5f1813fc523722c6331c2d6ed65555439fa14ff
Norbert de Langen <ndelangen@me.com>
2026-01-15 15:15:34 +0100

    test

--- scripts/ci/main.ts
+++ scripts/ci/main.ts
@@ -91,7 +91,7 @@ function generateConfig(workflow: Workflow) {
    * const filteredTodos = todos.filter((job) => !!job.id.includes('qwik'));
    * ```
    */
-  const filteredJobs = jobs.filter((job) => !!job);
+  const filteredJobs = jobs.filter((job) => !!job.name.includes('prerelease'));
 
   const isDebugging = filteredJobs.length !== jobs.length;
 

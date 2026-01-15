59ccb519c1f0de656c713ac3bcb91d602a1125af
yannbf <yannbf@gmail.com>
2026-01-15 16:51:04 +0100

    Core: Improve addon sanitization

--- code/core/src/telemetry/storybook-metadata.test.ts
+++ code/core/src/telemetry/storybook-metadata.test.ts
@@ -150,8 +150,8 @@ describe('storybook-metadata', () => {
         '@storybook/addon-knobs',
         '@storybook/addon-ends-with-js',
         '@storybook/addon-postcss',
-        '../local-addon',
-        '../../',
+        'CUSTOM:local-addon',
+        'CUSTOM:..',
       ]);
     });
 
@@ -162,7 +162,7 @@ describe('storybook-metadata', () => {
       cwdSpy = vi.spyOn(process, `cwd`).mockReturnValueOnce(cwdMockPath);
 
       expect(sanitizeAddonName(`${cwdMockPath}\\local-addon\\themes.js`)).toEqual(
-        '$SNIP\\local-addon\\themes'
+        'CUSTOM:local-addon'
       );
     });
 
@@ -173,8 +173,80 @@ describe('storybook-metadata', () => {
       cwdSpy = vi.spyOn(process, `cwd`).mockReturnValue(cwdMockPath);
 
       expect(sanitizeAddonName(`${cwdMockPath}/local-addon/themes.js`)).toEqual(
-        '$SNIP/local-addon/themes'
+        'CUSTOM:local-addon'
       );
+    });
+
+    describe('normalizes path-like addon definitions', () => {
+      it('node_modules and package-manager cache or pnp like paths', () => {
+        const snippedAddonNames = [
+          '$SNIP/node_modules/@storybook/addon-docs',
+          '$SNIP\\node_modules\\@storybook\\addon-docs',
+          '$SNIP/node_modules/.pnpm/@storybook+addon-a11y@10.0.8_storybook@10.0.8_@testing-library+dom@10.4.0_prettier@3.7._4a81ac32f3a0cc0e4b95fdb0fa907a4f/node_modules/@storybook/addon-a11y',
+          '$SNIP\\node_modules\\.pnpm\\@storybook+addon-onboarding_2f3ab626f8b157d8b10cc93b0c4f7171\\node_modules\\@storybook\\addon-onboarding',
+          '$SNIP/common/temp/node_modules/.pnpm/@storybook+addon-essentials@8.5.8_storybook@8.5.8/node_modules/@storybook/addon-essentials',
+          '$SNIP/.yarn/__virtual__/@storybook-addon-jest-virtual-ceb31c55cc/0/cache/@storybook-addon-jest-npm-9.1.12-adf55af7e8-904699d820.zip/node_modules/@storybook/addon-jest',
+          '$SNIP\\.yarn\\__virtual__\\@storybook-addon-actions-virtual-ecf55a46d7\\4\\Users\\Foo\\AppData\\Local\\Yarn\\Berry\\cache\\@storybook-addon-actions-npm-8.6.7-0b3fcdd2b2-10.zip\\node_modules\\@storybook\\addon-actions',
+          '$SNIP/.yarn/cache/@storybook-addon-webpack5-compiler-babel-npm-4.0.0-7fea55bdc6-abe15a1cd3.zip/node_modules/@storybook/addon-webpack5-compiler-babel',
+        ].map(sanitizeAddonName);
+
+        expect(snippedAddonNames).toEqual([
+          '@storybook/addon-docs',
+          '@storybook/addon-docs',
+          '@storybook/addon-a11y',
+          '@storybook/addon-onboarding',
+          '@storybook/addon-essentials',
+          '@storybook/addon-jest',
+          '@storybook/addon-actions',
+          '@storybook/addon-webpack5-compiler-babel',
+        ]);
+
+        const filePathAddonNames = [
+          '/tmp/yarn_node_modules/7/1fb08592505aa9425e2d6d3ffdc05d84087e575a/yarn_install_node_modules/js/packages/config-storybook/node_modules/@storybook/addon-links/dist/cjs/index.js',
+          '/node_modules/@chromatic-com/storybook',
+          '../../../@storybook/addon-links',
+          '/C:/Users/foo/OneDrive%20-%20BAR%20BAZ/Desktop/project/node_modules/@storybook/addon-coverage',
+          'C:\\Users\\Foo\\AppData\\Local\\Yarn\\Berry\\cache\\@storybook-addon-measure-npm-7.6.21-61d2a610cb-10.zip\\node_modules\\@storybook\\addon-measure',
+          '/home/foo/project/node_modules/.pnpm/@storybook+addon-docs@10.0.2_@types+react@19.2.2_esbuild@0.25.10_rollup@4.31.0_storyboo_7cb8a1f4d4ca81d0abdb0f0cfacb0423/node_modules/@storybook/addon-docs',
+          '/home/foo/.yarn/berry/cache/@storybook-addon-interactions-npm-7.6.1-9e0ac1ff40-10.zip/node_modules/@storybook/addon-interactions',
+          '/Users/foo/project/node_modules/@storybook/addon-links/dist/cjs/index.js',
+        ].map(sanitizeAddonName);
+
+        expect(filePathAddonNames).toEqual([
+          '@storybook/addon-links',
+          '@chromatic-com/storybook',
+          '@storybook/addon-links',
+          '@storybook/addon-coverage',
+          '@storybook/addon-measure',
+          '@storybook/addon-docs',
+          '@storybook/addon-interactions',
+          '@storybook/addon-links',
+        ]);
+      });
+
+      it('custom, local, or non-node_modules paths', () => {
+        const addonNames = [
+          'file://$SNIP/.storybook/redesign-addon',
+          '$SNIP/addons/airtracker',
+          '$SNIP/preset/index',
+          '$SNIP/src',
+          '../../../tools/storybook/src/plugins/storybook-translations',
+          './html-addon',
+          '..',
+          'file:///D:/foo/templates/.storybook/addons/some-addon',
+        ].map(sanitizeAddonName);
+
+        expect(addonNames).toEqual([
+          'CUSTOM:redesign-addon',
+          'CUSTOM:airtracker',
+          'CUSTOM:preset',
+          'CUSTOM:src',
+          'CUSTOM:storybook-translations',
+          'CUSTOM:html-addon',
+          'CUSTOM:..',
+          'CUSTOM:some-addon',
+        ]);
+      });
     });
   });
 

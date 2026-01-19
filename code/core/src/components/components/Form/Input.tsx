86629a55dd56f7059d5c81edf39bbb836db589b8
Gert Hengeveld <info@ghengeveld.nl>
2026-01-19 14:22:15 +0100

    Render unit as unselectable suffix on input

--- code/core/src/components/components/Form/Input.tsx
+++ code/core/src/components/components/Form/Input.tsx
@@ -13,6 +13,33 @@ import {
   validation,
 } from './styles';
 
+const Wrapper = styled.div({
+  position: 'relative',
+  width: '100%',
+});
+
+const Mask = styled.div(
+  ({ theme }) => ({
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    display: 'flex',
+    alignItems: 'center',
+    pointerEvents: 'none',
+    overflow: 'hidden',
+    color: theme.textMutedColor,
+    'span:first-of-type': {
+      opacity: 0,
+    },
+  }),
+  (props) => {
+    const { fontSize, lineHeight, padding } = styles(props);
+    return { fontSize, lineHeight, padding };
+  }
+);
+
 type InputProps = Omit<
   HTMLProps<HTMLInputElement>,
   keyof {
@@ -26,12 +53,26 @@ type InputProps = Omit<
   align?: Alignments;
   valid?: ValidationStates;
   height?: number;
+  suffix?: string;
 };
 
 export const Input = Object.assign(
   styled(
-    forwardRef<any, InputProps>(function Input({ size, valid, align, ...props }, ref) {
-      return <input {...props} ref={ref} />;
+    forwardRef<any, InputProps>(function Input(
+      { size, valid, align, value, suffix, ...props },
+      ref
+    ) {
+      return (
+        <Wrapper>
+          <input {...props} value={value} ref={ref} />
+          {suffix && (
+            <Mask aria-hidden>
+              <span>{value}</span>
+              <span>{suffix}</span>
+            </Mask>
+          )}
+        </Wrapper>
+      );
     })
   )<InputProps>(styles, sizes, alignment, validation, {
     minHeight: 32,

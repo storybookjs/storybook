9b524332117d8f1e379c73a5483af28acbb4ad5f
Steve Dodier-Lazaro <Sidnioulz@users.noreply.github.com>
2026-01-19 14:17:23 +0100

    Apply suggestion from @Sidnioulz

--- code/addons/docs/src/blocks/components/Preview.tsx
+++ code/addons/docs/src/blocks/components/Preview.tsx
@@ -191,10 +191,6 @@ const RelativeActionBar = styled(ActionBar)({
  * A preview component for showing one or more component `Story` items. The preview also shows the
  * source for the component as a drop-down.
  */
-/**
- * A preview component for showing one or more component `Story` items. The preview also shows the
- * source for the component as a drop-down.
- */
 export const Preview: FC<PreviewProps> = ({
   isLoading,
   isColumn,

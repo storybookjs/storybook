fec453aeac7537f6e227a66ce6f745feaf6cb777
Steve Dodier-Lazaro <sidnioulz@gmail.com>
2026-01-23 12:02:15 +0100

    Commit to estimate weight of Primer on build bundle

--- code/core/src/manager/components/sidebar/Explorer.tsx
+++ code/core/src/manager/components/sidebar/Explorer.tsx
@@ -1,10 +1,13 @@
 import type { FC } from 'react';
 import React, { useRef } from 'react';
 
-import { HighlightStyles } from './HighlightStyles';
-import { Ref } from './Refs';
+import { TreeView } from '@primer/react';
+
+// import { HighlightStyles } from './HighlightStyles';
+// import { Ref } from './Refs';
 import type { CombinedDataset, Selection } from './types';
-import { useHighlighted } from './useHighlighted';
+
+// import { useHighlighted } from './useHighlighted';
 
 export interface ExplorerProps {
   isLoading: boolean;
@@ -26,22 +29,36 @@ export const Explorer: FC<ExplorerProps> = React.memo(function Explorer({
   const containerRef = useRef<HTMLDivElement>(null);
 
   // Track highlighted nodes, keep it in sync with props and enable keyboard navigation
-  const [highlighted, setHighlighted, highlightedRef] = useHighlighted({
-    containerRef,
-    isLoading,
-    isBrowsing,
-    selected,
-  });
+  // const [highlighted, setHighlighted, highlightedRef] = useHighlighted({
+  //   containerRef,
+  //   isLoading,
+  //   isBrowsing,
+  //   selected,
+  // });
 
   return (
     <div
       ref={containerRef}
       id="storybook-explorer-tree"
-      data-highlighted-ref-id={highlighted?.refId}
-      data-highlighted-item-id={highlighted?.itemId}
+      // data-highlighted-ref-id={highlighted?.refId}
+      // data-highlighted-item-id={highlighted?.itemId}
     >
-      {highlighted && <HighlightStyles {...highlighted} />}
-      {dataset.entries.map(([refId, ref]) => (
+      {/* {highlighted && <HighlightStyles {...highlighted} />} */}
+
+      <TreeView aria-label="Files changed">
+        <TreeView.Item id="parent-node-one" defaultExpanded>
+          Parent node
+          <TreeView.SubTree>
+            <TreeView.Item id="child-node-one">Child node one</TreeView.Item>
+            <TreeView.Item id="child-node-two" current>
+              Child node two
+            </TreeView.Item>
+          </TreeView.SubTree>
+        </TreeView.Item>
+        <TreeView.Item id="sibling-node-one">Sibling node one</TreeView.Item>
+        <TreeView.Item id="sibling-node-two">Sibling node two</TreeView.Item>
+      </TreeView>
+      {/* {dataset.entries.map(([refId, ref]) => (
         <Ref
           {...ref}
           key={refId}
@@ -53,7 +70,7 @@ export const Explorer: FC<ExplorerProps> = React.memo(function Explorer({
           highlightedRef={highlightedRef}
           setHighlighted={setHighlighted}
         />
-      ))}
+      ))} */}
     </div>
   );
 });

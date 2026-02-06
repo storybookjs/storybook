520d722ee8075ff333aec50a45e3389bbecdba1c
Norbert de Langen <ndelangen@me.com>
2026-02-06 09:05:49 +0100

    Merge pull request #33766 from storybookjs/norbert/share-channel-events
    Telemetry: Add share events
    (cherry picked from commit ce42f59d76106c12957a18439e03030854497f4d)

--- code/core/src/core-server/presets/common-preset.ts
+++ code/core/src/core-server/presets/common-preset.ts
@@ -34,7 +34,7 @@ import { initCreateNewStoryChannel } from '../server-channel/create-new-story-ch
 import { initFileSearchChannel } from '../server-channel/file-search-channel';
 import { initGhostStoriesChannel } from '../server-channel/ghost-stories-channel';
 import { initOpenInEditorChannel } from '../server-channel/open-in-editor-channel';
-import { initPreviewInitializedChannel } from '../server-channel/preview-initialized-channel';
+import { initTelemetryChannel } from '../server-channel/telemetry-channel';
 import { initializeChecklist } from '../utils/checklist';
 import { defaultFavicon, defaultStaticDirs } from '../utils/constants';
 import { initializeSaveStory } from '../utils/save-story/save-story';
@@ -264,7 +264,7 @@ export const experimental_serverChannel = async (
   initCreateNewStoryChannel(channel, options, coreOptions);
   initGhostStoriesChannel(channel, options, coreOptions);
   initOpenInEditorChannel(channel, options, coreOptions);
-  initPreviewInitializedChannel(channel, options, coreOptions);
+  initTelemetryChannel(channel, options);
 
   return channel;
 };

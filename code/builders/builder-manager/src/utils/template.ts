e197865f797eabdab8eb937cb12786ce6c72f1d4
yannbf <yannbf@gmail.com>
2026-02-16 12:17:29 +0100

    Core: Require token for websocket connections

--- code/builders/builder-manager/src/utils/template.ts
+++ code/builders/builder-manager/src/utils/template.ts
@@ -34,25 +34,33 @@ export const renderHTML = async (
   refs: Promise<Record<string, Ref>>,
   logLevel: Promise<string>,
   docsOptions: Promise<DocsOptions>,
-  { versionCheck, previewUrl, configType }: Options
+  { versionCheck, previewUrl, configType, presets }: Options
 ) => {
   const titleRef = await title;
   const templateRef = await template;
+
+  const globals: Record<string, any> = {
+    FEATURES: JSON.stringify(await features, null, 2),
+    REFS: JSON.stringify(await refs, null, 2),
+    LOGLEVEL: JSON.stringify(await logLevel, null, 2),
+    DOCS_OPTIONS: JSON.stringify(await docsOptions, null, 2),
+    CONFIG_TYPE: JSON.stringify(await configType, null, 2),
+    // These two need to be double stringified because the UI expects a string
+    VERSIONCHECK: JSON.stringify(JSON.stringify(versionCheck), null, 2),
+    PREVIEW_URL: JSON.stringify(previewUrl, null, 2), // global preview URL
+  };
+
+  if (configType === 'DEVELOPMENT') {
+    const channelOptions = await presets.apply('core');
+    // Manager only needs the token currently, so we don't pass any other channel options.
+    globals.CHANNEL_OPTIONS = { wsToken: channelOptions?.wsToken };
+  }
 
   return render(templateRef, {
     title: titleRef ? `${titleRef} - Storybook` : 'Storybook',
     files: { js: jsFiles, css: cssFiles },
     favicon: await favicon,
-    globals: {
-      FEATURES: JSON.stringify(await features, null, 2),
-      REFS: JSON.stringify(await refs, null, 2),
-      LOGLEVEL: JSON.stringify(await logLevel, null, 2),
-      DOCS_OPTIONS: JSON.stringify(await docsOptions, null, 2),
-      CONFIG_TYPE: JSON.stringify(await configType, null, 2),
-      // These two need to be double stringified because the UI expects a string
-      VERSIONCHECK: JSON.stringify(JSON.stringify(versionCheck), null, 2),
-      PREVIEW_URL: JSON.stringify(previewUrl, null, 2), // global preview URL
-    },
+    globals,
     head: (await customHead) || '',
   });
 };

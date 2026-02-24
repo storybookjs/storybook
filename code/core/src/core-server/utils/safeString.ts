caa833c89b4b6b3bc9a4a7f16869cb0e39607013
Valentin Palkovic <valentin@chromatic.com>
2026-02-24 10:58:08 +0100

    Fix formatting

--- code/core/src/core-server/utils/safeString.ts
+++ code/core/src/core-server/utils/safeString.ts
@@ -1,15 +1,14 @@
-
-/**
- * Escape special characters in a string to make it safe for use in JavaScript strings.
- */
+/** Escape special characters in a string to make it safe for use in JavaScript strings. */
 export function safeJsString(str: string): string {
-  return str.replace(/['"\\\b\f\n\r\t]/g, (char) => {
+  return str.replace(/['"\\\b\f\n\r\t`$]/g, (char) => {
     const codes: Record<string, string> = {
       "'": "\\'",
       '"': '\\"',
-      "\\": "\\\\",
-      "\n": "\\n",
-      "\r": "\\r"
+      '\\': '\\\\',
+      '\n': '\\n',
+      '\r': '\\r',
+      '`': '\\`',
+      $: '\\$',
     };
     return codes[char] || char;
   });

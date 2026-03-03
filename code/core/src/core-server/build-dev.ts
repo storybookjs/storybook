dd5bfac3a453eb449409498cabd5627b98b04f5f
Gert Hengeveld <info@ghengeveld.nl>
2026-03-03 14:54:59 +0100

    Show allowed hosts in starup information

--- code/core/src/core-server/build-dev.ts
+++ code/core/src/core-server/build-dev.ts
@@ -320,6 +320,7 @@ export async function buildDevStandalone(
         name,
         address: localAddress,
         networkAddress,
+        allowedHosts,
         managerTotalTime,
         previewTotalTime,
       });

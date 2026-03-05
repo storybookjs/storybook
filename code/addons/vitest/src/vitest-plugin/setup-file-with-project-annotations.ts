933e0b769f3b96be730763bd38937a975b3b8381
Valentin Palkovic <valentin@chromatic.com>
2026-03-05 14:55:19 +0100

    Refactor Vitest setup to remove the need for a dedicated setup file and update documentation accordingly

--- code/addons/vitest/src/vitest-plugin/setup-file-with-project-annotations.ts
+++ code/addons/vitest/src/vitest-plugin/setup-file-with-project-annotations.ts
@@ -1,5 +1,6 @@
+import { setProjectAnnotations } from 'storybook/internal/preview-api';
+
 // @ts-expect-error - virtual module provided by storybook-project-annotations-plugin
 import { getProjectAnnotations } from 'virtual:/@storybook/builder-vite/project-annotations.js';
 
-// Load project annotations from the virtual module provided by the Storybook Vite plugin
-globalThis.globalProjectAnnotations = getProjectAnnotations();
+setProjectAnnotations(getProjectAnnotations());

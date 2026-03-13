6d132fffe09d5ac72117879e0a75a3e8236e82eb
Yann Braga <yannbf@gmail.com>
2026-03-13 13:45:48 +0100

    Merge pull request #34112 from storybookjs/sidnioulz/update-node-executors-angular
    Angular: Update Node CI executors to support Angular prerelease Node.js min version
    (cherry picked from commit a61002df20c906ca4c000e0e178ed78b39c831ad)

--- scripts/utils/yarn.ts
+++ scripts/utils/yarn.ts
@@ -37,9 +37,9 @@ export const addPackageResolutions = async ({ cwd, dryRun }: YarnOptions) => {
     ...packageJson.resolutions,
     ...storybookVersions,
     // this is for our CI test, ensure we use the same version as docker image, it should match version specified in `./code/package.json` and `.circleci/config.yml`
-    playwright: '1.52.0',
-    'playwright-core': '1.52.0',
-    '@playwright/test': '1.52.0',
+    playwright: '1.58.2',
+    'playwright-core': '1.58.2',
+    '@playwright/test': '1.58.2',
   };
   await writeFile(packageJsonPath, JSON.stringify(packageJson, null, 2));
 };

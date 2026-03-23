d38f38c59a77d8d6d8b8e0a7cac2e07b0f5438ca
Kai Röder <roeder.dev@gmail.com>
2026-03-23 20:47:56 +0100

    Apply corerabbit suggestion

--- code/e2e-tests/search.spec.ts
+++ code/e2e-tests/search.spec.ts
@@ -1,30 +1,30 @@
 import { expect, test } from '@playwright/test';
 import process from 'process';
 
-import { SbPage } from './util';
-
 const storybookUrl = process.env.STORYBOOK_URL || 'http://localhost:8001';
 
 test.describe('searching', () => {
   test('a non-docs story renders the label a11y friendly', async ({ page }) => {
     await page.goto(`${storybookUrl}`);
     await page.getByRole('searchbox').fill('Primary');
-    await expect(
-      page.getByRole('option', {
-        name: 'Example, Button, Primary',
-        exact: true,
-      })
-    ).toBeVisible();
+    const option = page.getByRole('option', {
+      name: 'Example, Button, Primary',
+      exact: true,
+    });
+
+    await expect(option).toBeVisible();
+    await expect(option.locator('[aria-hidden="true"]')).toBeVisible();
   });
 
   test('a docs story renders the label a11y friendly', async ({ page }) => {
     await page.goto(`${storybookUrl}`);
     await page.getByRole('searchbox').fill('Docs');
-    await expect(
-      page.getByRole('option', {
-        name: 'Docs, Configure your project',
-        exact: true,
-      })
-    ).toBeVisible();
+    const option = page.getByRole('option', {
+      name: 'Docs, Configure your project',
+      exact: true,
+    });
+
+    await expect(option).toBeVisible();
+    await expect(option.locator('[aria-hidden="true"]')).toBeVisible();
   });
 });

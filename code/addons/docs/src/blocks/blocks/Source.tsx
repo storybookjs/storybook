04727505459c5930a60d43fb66f8c954e81a8d7a
Andrei Chmelev <an.chmelev@gmail.com>
2026-03-26 04:28:57 +0300

    Addon-Docs: Simplify Source block implementation

--- code/addons/docs/src/blocks/blocks/Source.tsx
+++ code/addons/docs/src/blocks/blocks/Source.tsx
@@ -174,22 +174,17 @@ export const useSourceProps = (
  * Story source doc block renders source code if provided, or the source for a story if `storyId` is
  * provided, or the source for the current story if nothing is provided.
  */
-const SourceWithStorySnippet = (props: SourceProps) => {
+const SourceImpl = (props: SourceProps) => {
   const sourceContext = useContext(SourceContext);
   const docsContext = useContext(DocsContext);
-  const sourceProps = useSourceProps(props, docsContext, sourceContext);
-  return <PureSource {...sourceProps} />;
-};
-
-const SourceWithCode = (props: SourceProps) => {
-  const docsContext = useContext(DocsContext);
-  const sourceProps = useSourceProps(props, docsContext, EMPTY_SOURCE_CONTEXT);
-  return <PureSource {...sourceProps} />;
-};
-
-const SourceImpl = (props: SourceProps) => {
   const hasCodeProp = props.code !== undefined;
-  return hasCodeProp ? <SourceWithCode {...props} /> : <SourceWithStorySnippet {...props} />;
+  const sourceProps = useSourceProps(
+    props,
+    docsContext,
+    hasCodeProp ? EMPTY_SOURCE_CONTEXT : sourceContext
+  );
+
+  return <PureSource {...sourceProps} />;
 };
 
 export const Source = withMdxComponentOverride('Source', SourceImpl);

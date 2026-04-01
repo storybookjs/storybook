dc24eb75f2e0e1ad5d82d138a7ef3dd31f8ce627
Steve Dodier-Lazaro <sidnioulz@gmail.com>
2026-04-01 12:55:34 +0200

    refactor: use shared constants and add declared version bump in create-storybook bin entry

--- code/lib/create-storybook/src/bin/index.ts
+++ code/lib/create-storybook/src/bin/index.ts
@@ -1,20 +1,27 @@
 #!/usr/bin/env node
-import { logger } from 'storybook/internal/node-logger';
+export {};
 
-import { dedent } from 'ts-dedent';
+try {
+  const { isNodeVersionSupported, MIN_SUPPORTED_NODE_DESCRIPTION } =
+    await import('storybook/internal/common');
+  const [major, minor, patch] = process.versions.node.split('.').map(Number);
 
-const [majorNodeVersion, minorNodeVersion] = process.versions.node.split('.').map(Number);
+  if (!isNodeVersionSupported(major, minor, patch)) {
+    try {
+      const { handleUnsupportedNodeRuntime } = await import('../node-version-check.ts');
+      await handleUnsupportedNodeRuntime(major, minor, patch);
+    } catch {
+      const { logger } = await import('storybook/internal/node-logger');
+      logger.error(
+        `To run Storybook, you need Node.js version ${MIN_SUPPORTED_NODE_DESCRIPTION}.\n` +
+          `You are currently running Node.js ${process.version}.`
+      );
+    }
+    process.exit(1);
+  }
 
-if (
-  majorNodeVersion < 20 ||
-  (majorNodeVersion === 20 && minorNodeVersion < 19) ||
-  (majorNodeVersion === 22 && minorNodeVersion < 12)
-) {
-  logger.error(
-    dedent`To run Storybook, you need Node.js version 20.19+ or 22.12+.
-      You are currently running Node.js ${process.version}. Please upgrade your Node.js installation.`
-  );
+  await import('./run.ts');
+} catch {
+  console.error(`Storybook does not support Node.js ${process.version}. Please upgrade.`);
   process.exit(1);
 }
-
-import('./run.ts');

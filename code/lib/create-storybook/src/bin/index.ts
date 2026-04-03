25cd8f02869ed4b3b811cde64cfe9b729f90e6df
Steve Dodier-Lazaro <sidnioulz@gmail.com>
2026-04-03 15:34:40 +0200

    Show higher quality error message when possible

--- code/lib/create-storybook/src/bin/index.ts
+++ code/lib/create-storybook/src/bin/index.ts
@@ -2,26 +2,30 @@
 export {};
 
 try {
-  const { isNodeVersionSupported, MIN_SUPPORTED_NODE_DESCRIPTION } =
-    await import('storybook/internal/common');
+  const { isNodeVersionSupported } = await import('storybook/internal/common');
   const [major, minor, patch] = process.versions.node.split('.').map(Number);
 
   if (!isNodeVersionSupported(major, minor, patch)) {
-    try {
-      const { handleUnsupportedNodeRuntime } = await import('../node-version-check.ts');
-      await handleUnsupportedNodeRuntime(major, minor, patch);
-    } catch {
-      const { logger } = await import('storybook/internal/node-logger');
-      logger.error(
-        `To run Storybook, you need Node.js version ${MIN_SUPPORTED_NODE_DESCRIPTION}.\n` +
-          `You are currently running Node.js ${process.version}.`
-      );
-    }
+    const { handleUnsupportedNodeRuntime } = await import('../node-version-check.ts');
+    await handleUnsupportedNodeRuntime(major, minor, patch);
     process.exit(1);
   }
-
-  await import('./run.ts');
 } catch {
-  console.error(`Storybook does not support Node.js ${process.version}. Please upgrade.`);
-  process.exit(1);
+  try {
+    const { MIN_SUPPORTED_NODE_DESCRIPTION } = await import('storybook/internal/common');
+    const { logger } = await import('storybook/internal/node-logger');
+    logger.error(
+      `To run Storybook, you need Node.js version ${MIN_SUPPORTED_NODE_DESCRIPTION}.\n` +
+        `You are currently running Node.js ${process.version}.`
+    );
+  } catch {
+    // Back up in case Node version is so old that dynamic imports or the logger fail to run.
+    console.error(
+      `\nStorybook does not support Node.js ${process.version}. Please upgrade to run Storybook.\n`
+    );
+  } finally {
+    process.exit(1);
+  }
 }
+
+await import('./run.ts');

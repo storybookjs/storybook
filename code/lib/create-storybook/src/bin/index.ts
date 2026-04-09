a8bc7b3f3477f1402faf50ef8bffc59dfb6979a2
Steve Dodier-Lazaro <sidnioulz@gmail.com>
2026-04-09 13:10:01 +0200

    Remove backup node version warning as old nodes crash earlier

--- code/lib/create-storybook/src/bin/index.ts
+++ code/lib/create-storybook/src/bin/index.ts
@@ -1,25 +1,17 @@
 #!/usr/bin/env node
 export {};
 
-try {
-  const { isNodeVersionSupported } = await import('storybook/internal/common');
-  const [major, minor, patch] = process.versions.node.split('.').map(Number);
+const { isNodeVersionSupported } = await import('storybook/internal/common');
+const [major, minor, patch] = process.versions.node.split('.').map(Number);
 
-  if (!isNodeVersionSupported(major, minor, patch)) {
-    const { MIN_SUPPORTED_NODE_DESCRIPTION } = await import('storybook/internal/common');
-    const { logger } = await import('storybook/internal/node-logger');
-    logger.error(
-      `To run Storybook, you need Node.js version ${MIN_SUPPORTED_NODE_DESCRIPTION}.\n` +
-        `You are currently running Node.js ${process.version}.`
-    );
-    logger.outro('');
-    process.exit(1);
-  }
-} catch {
-  // Back up in case Node version is so old that dynamic imports or the logger fail to run.
-  console.error(
-    `\nStorybook does not support Node.js ${process.version}. Please upgrade to run Storybook.\n`
+if (!isNodeVersionSupported(major, minor, patch)) {
+  const { MIN_SUPPORTED_NODE_DESCRIPTION } = await import('storybook/internal/common');
+  const { logger } = await import('storybook/internal/node-logger');
+  logger.error(
+    `To run Storybook, you need Node.js version ${MIN_SUPPORTED_NODE_DESCRIPTION}.\n` +
+      `You are currently running Node.js ${process.version}.`
   );
+  logger.outro('');
   process.exit(1);
 }
 

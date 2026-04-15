bcdfeb22bb37ec004110b5773c44e4b67e43ebbb
Steve Dodier-Lazaro <sidnioulz@gmail.com>
2026-04-15 09:58:07 +0200

    Repair global exports mashed up by merge resolution

--- code/core/src/manager/globals/exports.ts
+++ code/core/src/manager/globals/exports.ts
@@ -574,9 +574,9 @@ export default {
     'withReset',
   ],
   'storybook/internal/core-events': [
+    'AI_PROMPT_NUDGE',
     'AI_SETUP_ANALYTICS_REQUEST',
     'AI_SETUP_ANALYTICS_RESPONSE',
-    'AI_PROMPT_NUDGE',
     'ARGTYPES_INFO_REQUEST',
     'ARGTYPES_INFO_RESPONSE',
     'CHANNEL_CREATED',

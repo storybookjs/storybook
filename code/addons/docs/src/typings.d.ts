e9b847706509553badb0f606b2e25e424f04341c
ia319 <linglingmeng876@gmail.com>
2026-04-24 01:45:29 +0800

    fix(addons-docs): make docs context type match usage

--- code/addons/docs/src/typings.d.ts
+++ code/addons/docs/src/typings.d.ts
@@ -4,8 +4,7 @@ declare module 'vue/dist/vue';
 
 declare var FEATURES: import('storybook/internal/types').StorybookConfigRaw['features'];
 declare var __DOCS_CONTEXT__: import('react').Context<
-  | import('storybook/internal/types').DocsContextProps<import('storybook/internal/types').Renderer>
-  | null
+  import('storybook/internal/types').DocsContextProps<import('storybook/internal/types').Renderer>
 >;
 declare var PREVIEW_URL: string | undefined;
 declare var LOGLEVEL: 'trace' | 'debug' | 'info' | 'warn' | 'error' | 'silent' | undefined;

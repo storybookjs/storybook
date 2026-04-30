41577f8785886f8232b9f6a5747bd87857d70b65
Julien Huang <julien.h.dev@gmail.com>
2026-04-30 17:54:59 +0200

    fix: decorator order

--- code/frameworks/tanstack-react/src/preview.tsx
+++ code/frameworks/tanstack-react/src/preview.tsx
@@ -1,4 +1,5 @@
 import type { DecoratorFunction, LoaderFunction, Renderer } from 'storybook/internal/types';
+// @ts-expect-error untyped
 import { applyDecorators as reactApplyDecorators } from '@storybook/react/entry-preview-docs';
 
 import type { TanStackParameters } from './types.ts';
@@ -10,7 +11,12 @@ export const loaders: LoaderFunction<Renderer>[] = [routeComponentLoader];
 export const applyDecorators = (
   storyFn: Parameters<typeof reactApplyDecorators>[0],
   allDecorators: DecoratorFunction[]
-) => reactApplyDecorators(storyFn, [tanstackRouteDecorator, ...allDecorators]);
+) =>
+  // reorder decorators so `jsxDecorator` is innermost, and `tanstackRouteDecorator` is just outside it
+  reactApplyDecorators(storyFn, [
+    tanstackRouteDecorator as DecoratorFunction,
+    ...allDecorators,
+  ] as Parameters<typeof reactApplyDecorators>[1]);
 
 export const parameters: TanStackParameters = {
   tanstack: {},

9180514f9e4505781add7956415345ebb879c1c0
Ishant5436 <ishant.p@somaiya.edu>
2026-07-08 23:27:20 +0530

    test: add regression test for optional union types infer controls (#12641)

--- code/core/src/docs-tools/argTypes/convert/convert.test.ts
+++ code/core/src/docs-tools/argTypes/convert/convert.test.ts
@@ -214,6 +214,60 @@ describe('storybook type system', () => {
         }
       `);
     });
+    it('optional unions', () => {
+      const input = readFixture('typescript/optional-unions.tsx');
+      expect(input).toMatchInlineSnapshot(`
+        "import type { FC } from 'react';
+        import React from 'react';
+
+        type LargeUnion = 'k1' | 'k2' | 'k3' | 'k4' | 'k5' | 'k6' | 'k7' | 'k8' | 'k9' | 'k10' | 'k11' | 'k12' | 'k13' | 'k14' | 'k15' | 'k16' | 'k17' | 'k18' | 'k19' | 'k20' | 'k21' | 'k22' | 'k23' | 'k24' | 'k25' | 'k26' | 'k27' | 'k28' | 'k29';
+        interface Props {
+          largeOptionalUnion?: LargeUnion;
+        }
+        export const Component: FC<Props> = (props: Props) => <>JSON.stringify(props)</>;
+        "
+      `);
+      expect(convertTs(input)).toMatchInlineSnapshot(`
+        {
+          "largeOptionalUnion": {
+            "raw": "LargeUnion",
+            "name": "enum",
+            "value": [
+              "k1",
+              "k2",
+              "k3",
+              "k4",
+              "k5",
+              "k6",
+              "k7",
+              "k8",
+              "k9",
+              "k10",
+              "k11",
+              "k12",
+              "k13",
+              "k14",
+              "k15",
+              "k16",
+              "k17",
+              "k18",
+              "k19",
+              "k20",
+              "k21",
+              "k22",
+              "k23",
+              "k24",
+              "k25",
+              "k26",
+              "k27",
+              "k28",
+              "k29"
+            ]
+          }
+        }
+      `);
+    });
+
     it('intersections', () => {
       const input = readFixture('typescript/intersections.tsx');
       expect(input).toMatchInlineSnapshot(`

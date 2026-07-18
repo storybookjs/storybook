c776274943e5c6d456be305d8cc35282e52c3af4
awnoa <zerouali.bardai.omar@gmail.com>
2026-07-18 13:47:21 +0200

    Blocks: Make useGlobals independent of stories

--- code/addons/docs/src/blocks/blocks/useGlobals.ts
+++ code/addons/docs/src/blocks/blocks/useGlobals.ts
@@ -1,3 +1,30 @@
+import { useCallback, useContext, useEffect, useState } from 'react';
+
+import { GLOBALS_UPDATED, UPDATE_GLOBALS } from 'storybook/internal/core-events';
+import type { Globals } from 'storybook/internal/csf';
+
+import { DocsContext } from './DocsContext';
+
+export const useGlobals = (): [Globals, (globals: Globals) => void] => {
+  const context = useContext(DocsContext);
+  const [globals, setGlobals] = useState(context.getGlobals());
+
+  useEffect(() => {
+    const onGlobalsUpdated = (changed: { globals: Globals }) => {
+      setGlobals(changed.globals);
+    };
+    context.channel.on(GLOBALS_UPDATED, onGlobalsUpdated);
+    return () => context.channel.off(GLOBALS_UPDATED, onGlobalsUpdated);
+  }, [context.channel]);
+
+  const updateGlobals = useCallback(
+    (updatedGlobals: Globals) =>
+      context.channel.emit(UPDATE_GLOBALS, { globals: updatedGlobals }),
+    [context.channel]
+  );
+
+  return [globals, updateGlobals];
+};
 import { useEffect, useState } from 'react';
 
 import { GLOBALS_UPDATED } from 'storybook/internal/core-events';

abb5247671b683efb9487a3b54c92ec28d450d87
Julien Huang <julien.h.dev@gmail.com>
2026-07-28 16:35:33 +0200

    Core: Move docgen JSDoc parser into csf-tools

--- code/core/src/csf-tools/jsdoc.test.ts
+++ code/core/src/csf-tools/jsdoc.test.ts
@@ -2,7 +2,7 @@ import { expect, it } from 'vitest';
 
 import { dedent } from 'ts-dedent';
 
-import { extractJSDocInfo } from './jsdocTags.ts';
+import { extractJSDocInfo } from './jsdoc.ts';
 
 it('should extract @summary tag', () => {
   const code = dedent`description\n@summary\n my summary`;

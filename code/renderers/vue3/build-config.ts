92f3df64c94586b506cfdacbe879cd248a59a2fa
Julien Huang <julien.h.dev@gmail.com>
2026-07-30 10:53:52 +0200

    Vue: Move vue-component-meta as renderer internal aand implement docgen extraction

--- code/renderers/vue3/build-config.ts
+++ code/renderers/vue3/build-config.ts
@@ -37,6 +37,10 @@ const config: BuildEntries = {
         entryPoint: './src/docgen/docgen-worker.ts',
         dts: false,
       },
+      {
+        exportEntries: ['./internal/docgen-engine'],
+        entryPoint: './src/docgen/component-meta.ts',
+      },
     ],
   },
 };

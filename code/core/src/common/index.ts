647e982151f1bb4e15163b0d2a31c5a1022efda3
Norbert de Langen <ndelangen@me.com>
2026-08-11 10:35:09 +0200

    Merge pull request #35743 from storybookjs/norbert/revive-34415-file-aware-tsconfig
    React: Fix RDT tsconfig selection for Vite project references
    (cherry picked from commit 94365e13ca2c49195702c1c365353698d0d42abe)

--- code/core/src/common/index.ts
+++ code/core/src/common/index.ts
@@ -32,6 +32,7 @@ export * from './utils/remove.ts';
 export * from './utils/resolve-path-in-sb-cache.ts';
 export * from './utils/symlinks.ts';
 export * from './utils/template.ts';
+export * from './utils/tsconfig.ts';
 export * from './utils/validate-config.ts';
 export * from './utils/validate-configuration-files.ts';
 export * from './utils/satisfies.ts';

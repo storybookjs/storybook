1e61ec4702eadeba048f28892405039cbd313984
Julien Huang <julien.h.dev@gmail.com>
2026-08-17 11:36:56 +0200

    fix: type issue

--- code/renderers/vue3/src/entry-preview-docs.ts
+++ code/renderers/vue3/src/entry-preview-docs.ts
@@ -1,5 +1,5 @@
 import { sourceDecorator } from './docs/sourceDecorator.ts';
 
-const isDocgenServerEnabled = globalThis.FEATURES?.experimentalDocgenServer;
+const isDocgenServerEnabled = (globalThis as any).FEATURES?.experimentalDocgenServer;
 
 export const decorators = isDocgenServerEnabled ? [] : [sourceDecorator];

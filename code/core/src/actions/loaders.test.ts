9b11e694c306673421cd86cd27658bad67189e20
waleed <waleedmirza667@gmail.com>
2026-09-14 23:55:47 +0500

    Actions: Cover multi-argument mock calls in loaders test

--- code/core/src/actions/loaders.test.ts
+++ code/core/src/actions/loaders.test.ts
@@ -27,4 +27,15 @@ describe('loaders', () => {
     expect(channel.emit).toHaveBeenCalledOnce();
     expect(channel.emit.mock.calls[0][1].data.args).toBe(event);
   });
+
+  it('forwards multiple mock-call arguments in order', () => {
+    loaders[0]({ parameters: {} } as any);
+
+    const spy = fn().mockName('onClick');
+    const event = { type: 'click' };
+    spy(event, 'extra', 42);
+
+    expect(channel.emit).toHaveBeenCalledOnce();
+    expect(channel.emit.mock.calls[0][1].data.args).toEqual([event, 'extra', 42]);
+  });
 });

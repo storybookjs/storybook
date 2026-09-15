c34db454ed3ea20f53edcd0e29e3b28c7423f8fd
Cody Kaup <dbkaup@gmail.com>
2026-09-15 15:27:07 -0500

    Include project-annotations.json in preview-stats graph

--- code/builders/builder-vite/src/plugins/webpack-stats-plugin.ts
+++ code/builders/builder-vite/src/plugins/webpack-stats-plugin.ts
@@ -12,6 +12,12 @@ import {
   getOriginalVirtualModuleId,
   getResolvedVirtualModuleId,
 } from '../virtual-file-names.ts';
+import { VIRTUAL_ID as PROJECT_ANNOTATIONS_VIRTUAL_ID } from './storybook-project-annotations-plugin.ts';
+
+const STATS_VIRTUAL_FILES = new Set([
+  ...Object.values(SB_VIRTUAL_FILES),
+  PROJECT_ANNOTATIONS_VIRTUAL_ID,
+]);
 
 /*
  * Reason, Module are copied from chromatic types
@@ -46,7 +52,7 @@ function isUserCode(moduleName: string) {
   }
 
   // keep Storybook's virtual files because they import the story files, so they are essential to the module graph
-  if (Object.values(SB_VIRTUAL_FILES).includes(getOriginalVirtualModuleId(moduleName))) {
+  if (STATS_VIRTUAL_FILES.has(getOriginalVirtualModuleId(moduleName))) {
     return true;
   }
 
@@ -74,7 +80,7 @@ export function pluginWebpackStats({ workingDir }: WebpackStatsPluginOptions): W
     // ! to ensure that the stats file doesn't change between the versions
     // ! Turbosnap is also only compatible with the old virtual file names
     // ! the old virtual file names did not start with the obligatory \0 character
-    if (Object.values(SB_VIRTUAL_FILES).includes(getOriginalVirtualModuleId(filename))) {
+    if (STATS_VIRTUAL_FILES.has(getOriginalVirtualModuleId(filename))) {
       // We have to append a forward slash because otherwise we break turbosnap.
       // As soon as the chromatic-cli supports `virtual:` id's without a starting forward slash,
       // we can remove adding the forward slash here
